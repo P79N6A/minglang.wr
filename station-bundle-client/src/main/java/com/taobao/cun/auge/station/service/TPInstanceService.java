77d641908ae05be49ec62a79a50f1a3421977647
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-05-12 11:15:03 +0800

    add

--- station-bundle-client/src/main/java/com/taobao/cun/auge/station/service/TPInstanceService.java
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/station/service/TPInstanceService.java
@@ -1,5 +1,9 @@
 package com.taobao.cun.auge.station.service;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+import com.taobao.cun.auge.station.condition.ForcedCloseCondition;
 import com.taobao.cun.auge.station.condition.PartnerStationCondition;
 import com.taobao.cun.auge.station.dto.PartnerInstanceDto;
 
@@ -7,11 +11,12 @@ import com.taobao.cun.auge.station.dto.PartnerInstanceDto;
  * 合伙人实例服务
  * 
  * 合伙人每参与一次村淘，都会生成一个实例。
+ * 
  * @author haihu.fhh
  *
  */
 public interface TPInstanceService {
-	
+
 	/**
 	 * 暂存人和村
 	 * 
@@ -19,7 +24,7 @@ public interface TPInstanceService {
 	 * @return
 	 */
 	public Long tempPartnerStation(PartnerStationCondition condition);
-	
+
 	/**
 	 * 正式提交，人和村
 	 * 
@@ -27,7 +32,7 @@ public interface TPInstanceService {
 	 * @return
 	 */
 	public Long submitPartnerStation(PartnerStationCondition condition);
-	
+
 	/**
 	 * 正式提交，暂存的人和村
 	 * 
@@ -35,8 +40,7 @@ public interface TPInstanceService {
 	 * @return
 	 */
 	public Long submitTempPartnerStation(PartnerStationCondition condition);
-	
-	
+
 	/**
 	 * 修改，人和村
 	 * 
@@ -44,8 +48,7 @@ public interface TPInstanceService {
 	 * @return
 	 */
 	public boolean updatePartnerStation(PartnerStationCondition condition);
-	
-	
+
 	/**
 	 * 删除合伙人实例
 	 * 
@@ -53,8 +56,7 @@ public interface TPInstanceService {
 	 * @return
 	 */
 	public boolean deletePartnerStation(Long instanceId);
-	
-	
+
 	/**
 	 * 签署入驻协议
 	 * 
@@ -62,8 +64,7 @@ public interface TPInstanceService {
 	 * @return
 	 */
 	public boolean signSettledProtocol(Long taobaoUserId);
-	
-	
+
 	/**
 	 * 签署管理协议
 	 * 
@@ -71,9 +72,73 @@ public interface TPInstanceService {
 	 * @return
 	 */
 	public boolean signManageProtocol(Long taobaoUserId);
-	
-	
-	public PartnerInstanceDto find(PartnerStationCondition condition);
-	
-	
+
+	/**
+	 * 冻结保证金
+	 * 
+	 * @param taobaoUserId
+	 * @param frozenMoney
+	 * @return
+	 */
+	public boolean freezeBond(Long taobaoUserId, BigDecimal frozenMoney);
+
+	/**
+	 * 开业
+	 * 
+	 * @param partnerInstanceId
+	 *            合伙人实例id
+	 * @param openDate
+	 *            开业时间
+	 * @param isImme
+	 *            是否立即开业
+	 * @param employeeId
+	 *            小二工号
+	 * @return
+	 */
+	public boolean openStation(Long partnerInstanceId, Date openDate, boolean isImme, String employeeId);
+
+	/**
+	 * 合伙人申请退出
+	 * 
+	 * @param taobaoUserId
+	 * @return
+	 */
+	public boolean quitApplyByPartner(Long taobaoUserId);
+
+	/**
+	 * 小二审批合伙人申请退出
+	 * 
+	 * @param partnerInstanceId
+	 * @param employeeId
+	 * @param isAgree
+	 * @return
+	 */
+	public boolean approvePartnerQuitApply(Long partnerInstanceId, String employeeId, boolean isAgree);
+
+	/**
+	 * 小二申请停业
+	 * 
+	 * @param forcedCloseCondition
+	 * @param employeeId
+	 * @return
+	 */
+	public boolean applyForcedClose(ForcedCloseCondition forcedCloseCondition, String employeeId);
+
+	/**
+	 * 小二申请撤点
+	 * 
+	 * @param forcedCloseCondition
+	 * @param employeeId
+	 * @return
+	 */
+	public boolean quitApplyByEmployee(Long partnerInstanceId, String employeeId);
+
+	/**
+	 * 详情，人和村。已脫敏
+	 * 
+	 * @param partnerStationId
+	 * @return
+	 */
+	public PartnerInstanceDto findSafedStationPartner(Long partnerStationId);
+
 }
