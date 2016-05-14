8633fa006c5ea0cd2b3dfa02ff097fc328de78a1
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-05-14 12:53:41 +0800

    add

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/PatnerInstanceServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/PatnerInstanceServiceImpl.java
@@ -7,14 +7,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.taobao.cun.auge.dal.domain.PartnerStationRel;
 import com.taobao.cun.auge.station.bo.PartnerInstanceBO;
 import com.taobao.cun.auge.station.bo.ProtocolBO;
+import com.taobao.cun.auge.station.bo.StationBO;
 import com.taobao.cun.auge.station.condition.ForcedCloseCondition;
 import com.taobao.cun.auge.station.condition.PartnerInstanceCondition;
 import com.taobao.cun.auge.station.enums.PartnerInstanceStateEnum;
 import com.taobao.cun.auge.station.enums.PartnerInstanceTypeEnum;
 import com.taobao.cun.auge.station.enums.ProtocolTargetBizTypeEnum;
 import com.taobao.cun.auge.station.enums.ProtocolTypeEnum;
+import com.taobao.cun.auge.station.enums.StationStatusEnum;
 import com.taobao.cun.auge.station.exception.AugeServiceException;
 import com.taobao.cun.auge.station.exception.enums.CommonExceptionEnum;
 import com.taobao.cun.auge.station.exception.enums.PartnerExceptionEnum;
@@ -34,6 +37,9 @@ public class PatnerInstanceServiceImpl implements PatnerInstanceService {
 
 	@Autowired
 	PartnerInstanceHandler partnerInstanceHandler;
+	
+	@Autowired
+	StationBO stationBO;
 
 	@Override
 	public Long addTemp(PartnerInstanceCondition condition) throws AugeServiceException {
@@ -128,19 +134,21 @@ public class PatnerInstanceServiceImpl implements PatnerInstanceService {
 			throws AugeServiceException {
 		try {
 			Long instanceId = forcedCloseCondition.getInstanceId();
-			int count = partnerInstanceBO.findChildPartners(instanceId, PartnerInstanceStateEnum.SERVICING);
-			if (count > 0) {
-				logger.error(StationExceptionEnum.HAS_CHILDREN_TPA.getDesc());
-				throw new AugeServiceException(StationExceptionEnum.HAS_CHILDREN_TPA);
-			}
+			PartnerStationRel partnerStationRel = partnerInstanceBO.findPartnerInstanceById(instanceId);
 
-			// partnerInstanceBO
-			//
-			// StationBO
+			// 校验是否还有下一级别的人。例如校验合伙人是否还存在淘帮手存在
+			partnerInstanceHandler.validateExistServiceChildren(
+					PartnerInstanceTypeEnum.valueof(partnerStationRel.getType()), instanceId);
 
+			// 合伙人实例停业中
+			partnerInstanceBO.changeState(instanceId, PartnerInstanceStateEnum.SERVICING,
+					PartnerInstanceStateEnum.CLOSING, employeeId);
+
+			// 村点停业中
+			stationBO.changeState(instanceId, StationStatusEnum.SERVICING, StationStatusEnum.CLOSING, employeeId);
+
+			// 通过事件，定时钟，启动停业流程
 			return true;
-
-			// 定时钟，启动停业流程
 
 		} catch (Exception e) {
 			logger.error(StationExceptionEnum.SIGN_SETTLE_PROTOCOL_FAIL.getDesc(), e);
