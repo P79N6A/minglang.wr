e79f5b29e87352862b4faf36cce3bf77e11ea0a4
张振环 <zhenhuan.zhangzh@alibaba-inc.com>
2016-05-16 00:23:44 +0800

    ci

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/Application.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/Application.java
@@ -4,24 +4,38 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.config.EnableIntegration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.taobao.cun.auge.station.dto.SampleInstance;
+import com.taobao.cun.auge.station.service.SampleInternalService;
 import com.taobao.hsf.app.spring.util.annotation.EnableHSFProvider;
 
 @SpringBootApplication
 @EnableHSFProvider
 @EnableTransactionManagement
+@EnableIntegration
 @IntegrationComponentScan
 public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
-    	try {
-    		SpringApplication application = new SpringApplication(Application.class);
-        	application.run(args);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    	ConfigurableApplicationContext ctx =
+				SpringApplication.run(Application.class, args);
+
+		SampleInternalService stationInternalService = ctx.getBean(SampleInternalService.class);
+		SampleInstance instance = new SampleInstance();
+		Long id = stationInternalService.addSample(instance);
+		System.out.println(id);
+		instance.setType("tp");
+		stationInternalService.routeSample(instance);
+		instance.setType("tpa");
+		stationInternalService.routeSample(instance);
+		instance.setType("tpv");
+		stationInternalService.routeSample(instance);
+		
+		stationInternalService.publishSubscribeSample(instance);
     	
     }
 
