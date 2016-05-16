643fbf903cf4a96da703a13caacf07590e051978
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-05-16 19:25:57 +0800

    fhh

---
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/event/domain/CuntaoFlowRecordEvent.java
@@ -0,0 +1,5 @@
+package com.taobao.cun.auge.event.domain;
+
+public class CuntaoFlowRecordEvent {
+
+}
---
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/event/listener/CuntaoFlowRecordListener.java
@@ -0,0 +1,16 @@
+package com.taobao.cun.auge.event.listener;
+
+import com.taobao.cun.crius.event.Event;
+import com.taobao.cun.crius.event.annotation.EventSub;
+import com.taobao.cun.crius.event.client.EventListener;
+
+@EventSub("cuntao-flow-record-event")
+public class CuntaoFlowRecordListener implements EventListener{
+
+	@Override
+	public void onMessage(Event arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
