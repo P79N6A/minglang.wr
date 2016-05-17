860b287fc714976e2e66de2f0f380f0eda43aee6
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-05-17 11:04:56 +0800

    add

---
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/station/dto/UserTagDto.java
@@ -0,0 +1,31 @@
+package com.taobao.cun.auge.station.dto;
+
+import java.io.Serializable;
+
+import com.taobao.cun.auge.station.enums.PartnerInstanceTypeEnum;
+
+public class UserTagDto implements Serializable {
+
+	private static final long serialVersionUID = -5842295776075501429L;
+
+	private Long taobaoUserId;
+
+	private PartnerInstanceTypeEnum partnerType;
+
+	public Long getTaobaoUserId() {
+		return taobaoUserId;
+	}
+
+	public void setTaobaoUserId(Long taobaoUserId) {
+		this.taobaoUserId = taobaoUserId;
+	}
+
+	public PartnerInstanceTypeEnum getPartnerType() {
+		return partnerType;
+	}
+
+	public void setPartnerType(PartnerInstanceTypeEnum partnerType) {
+		this.partnerType = partnerType;
+	}
+
+}
--- station-bundle-impl/src/main/java/com/taobao/cun/auge/event/listener/RemoveUserTagListener.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/event/listener/RemoveUserTagListener.java
@@ -1,11 +1,17 @@
 package com.taobao.cun.auge.event.listener;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.taobao.cun.auge.station.dto.UserTagDto;
 import com.taobao.cun.auge.station.enums.PartnerInstanceTypeEnum;
 import com.taobao.cun.auge.station.enums.StationStatusEnum;
+import com.taobao.cun.chronus.dto.GeneralTaskDto;
+import com.taobao.cun.chronus.enums.BusinessTypeEnum;
+import com.taobao.cun.chronus.service.TaskExecuteService;
 import com.taobao.cun.crius.event.Event;
 import com.taobao.cun.crius.event.annotation.EventSub;
 import com.taobao.cun.crius.event.client.EventListener;
@@ -25,44 +31,47 @@ public class RemoveUserTagListener implements EventListener {
 		Long taobaoUserId = (Long) map.get("taobaoUserId");
 		PartnerInstanceTypeEnum partnerType = (PartnerInstanceTypeEnum) map.get("partnerType");
 		
+		//监听已停业后，去标
 		if(StationStatusEnum.CLOSED.equals(status)){
 			submitRemoveUserTagTasks( taobaoUserId,  partnerType, operatorId);
 		}
 	}
 
-	private void submitRemoveUserTagTasks(Long taobaoUserId, PartnerInstanceTypeEnum partnerType,String operatorId) {
-//		// TODO 异构系统交互提交后台任务
-//		List<TaskVo> taskVos = new LinkedList<TaskVo>();
-//
-//		String businessNo = stationApplicationId.toString();// getBusinessNo();
-//		// TODO uic打标 begin
-//		TaskVo uicTaskVo = new TaskVo();
-//		uicTaskVo.setBusinessNo(businessNo);
-//		uicTaskVo.setBeanName("uicBo");
-//		uicTaskVo.setMethodName("removeUserTag");
-//		uicTaskVo.setBusinessStepNo(1l);
-//		uicTaskVo.setBusinessType(BusinessTypeEnum.STATION_QUITE_CONFIRM);
-//		uicTaskVo.setBusinessStepDesc("removeUserTag");
-//		uicTaskVo.setOperator(appId);
-//		uicTaskVo.setParameter(stationApplicationId);
-//		taskVos.add(uicTaskVo);
-//		// TODO uic打标 end
-//
-//		// TODO 旺旺打标 begin
-//		TaskVo wangwangTaskVo = new TaskVo();
-//		wangwangTaskVo.setBusinessNo(businessNo);
-//		wangwangTaskVo.setBeanName("aliWangWangBO");
-//		wangwangTaskVo.setMethodName("removeWangWangTag");
-//		wangwangTaskVo.setBusinessStepNo(2l);
-//		wangwangTaskVo.setBusinessType(BusinessTypeEnum.STATION_QUITE_CONFIRM);
-//		wangwangTaskVo.setBusinessStepDesc("removeWangWangTag");
-//		wangwangTaskVo.setOperator(appId);
-//		wangwangTaskVo.setParameter(stationApplicationId);
-//		taskVos.add(wangwangTaskVo);
-//		// TODO 旺旺打标 end
-//
-//		// TODO 提交任务
-//		taskExecuteService.submitTasks(taskVos, true);
+	private void submitRemoveUserTagTasks(Long taobaoUserId, PartnerInstanceTypeEnum partnerType, String operatorId) {
+		UserTagDto userTagDto = new UserTagDto();
+
+		userTagDto.setTaobaoUserId(taobaoUserId);
+		userTagDto.setPartnerType(partnerType);
+
+		List<GeneralTaskDto> taskLists = new LinkedList<GeneralTaskDto>();
+
+		//FIXME FHH 去标的服务未写，businessType未定义
+		// uic去标
+		GeneralTaskDto task = new GeneralTaskDto();
+		task.setBusinessNo(String.valueOf(taobaoUserId));
+		task.setBeanName("uicBo");
+		task.setMethodName("removeUserTag");
+		task.setBusinessStepNo(1l);
+		task.setBusinessType(BusinessTypeEnum.STATION_QUITE_CONFIRM);
+		task.setBusinessStepDesc("removeUserTag");
+		task.setOperator(operatorId);
+		task.setParameter(userTagDto);
+		taskLists.add(task);
+
+		// 旺旺去标
+		GeneralTaskDto wangwangTaskVo = new GeneralTaskDto();
+		wangwangTaskVo.setBusinessNo(String.valueOf(taobaoUserId));
+		wangwangTaskVo.setBeanName("aliWangWangBO");
+		wangwangTaskVo.setMethodName("removeWangWangTag");
+		wangwangTaskVo.setBusinessStepNo(2l);
+		wangwangTaskVo.setBusinessType(BusinessTypeEnum.STATION_QUITE_CONFIRM);
+		wangwangTaskVo.setBusinessStepDesc("removeWangWangTag");
+		wangwangTaskVo.setOperator(operatorId);
+		wangwangTaskVo.setParameter(userTagDto);
+		taskLists.add(wangwangTaskVo);
+
+		// 提交任务
+		taskExecuteService.submitTasks(taskLists);
 	}
 
 }
