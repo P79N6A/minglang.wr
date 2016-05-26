21375d88033393b94f3b3f1aab38a262e57abeef
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-05-26 12:23:38 +0800

    add

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/adapter/impl/UicReadAdapterImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/adapter/impl/UicReadAdapterImpl.java
@@ -1,11 +1,13 @@
 package com.taobao.cun.auge.station.adapter.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.taobao.cun.auge.station.adapter.UicReadAdapter;
 import com.taobao.cun.crius.common.resultmodel.ResultModel;
 import com.taobao.cun.crius.uic.service.CuntaoUicReadService;
 
+@Component
 public class UicReadAdapterImpl implements UicReadAdapter{
 
 	@Autowired
