f8b432cf732ac5d7d3a241334d28a26773a32aee
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-05-27 11:27:47 +0800

    ff

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/convert/PartnerInstanceConverter.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/convert/PartnerInstanceConverter.java
@@ -2,11 +2,10 @@ package com.taobao.cun.auge.station.convert;
 
 import java.util.List;
 
-import com.taobao.cun.auge.common.Converter;
 import com.taobao.cun.auge.dal.domain.PartnerInstance;
 import com.taobao.cun.auge.station.dto.PartnerInstanceDto;
 
-public final class PartnerInstanceConverter extends Converter{
+public final class PartnerInstanceConverter{
 	private PartnerInstanceConverter() {
 
 	}
