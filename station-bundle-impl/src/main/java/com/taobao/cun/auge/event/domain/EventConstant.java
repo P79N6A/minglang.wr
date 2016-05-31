5bb43848a8191d786dc80eb527bf3741fa149b3f
谨修 <jianke.ljk@alibaba-inc.com>
2016-05-31 17:20:13 +0800

    update

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/event/domain/EventConstant.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/event/domain/EventConstant.java
@@ -2,28 +2,34 @@ package com.taobao.cun.auge.event.domain;
 
 /**
  * 定义事件类型常量
+ * 
  * @author quanzhu.wangqz
  *
  */
 public final class EventConstant {
-	
-	private EventConstant(){
-		
+
+	private EventConstant() {
+
 	}
-	
+
 	/**
 	 * 合伙人服务站实例状态变更事件
 	 */
-	public static final String PARTNER_INSTANCE_STATE_CHANGE_EVENT="PARTNER_INSTANCE_STATE_CHANGE_EVENT";
-	
+	public static final String PARTNER_INSTANCE_STATE_CHANGE_EVENT = "PARTNER_INSTANCE_STATE_CHANGE_EVENT";
+
 	/**
 	 * 流程日志事件
 	 */
-	public static final String CUNTAO_FLOW_RECORD_EVENT="CUNTAO_FLOW_RECORD_EVENT";
-	
+	public static final String CUNTAO_FLOW_RECORD_EVENT = "CUNTAO_FLOW_RECORD_EVENT";
+
 	/**
 	 * 村点状态变更事件
 	 */
-	public static final String CUNTAO_STATION_STATUS_CHANGED_EVENT="CUNTAO_STATION_STATUS_CHANGED_EVENT";
+	public static final String CUNTAO_STATION_STATUS_CHANGED_EVENT = "CUNTAO_STATION_STATUS_CHANGED_EVENT";
+
+	/**
+	 * station_apply信息同步
+	 */
+	public static final String CUNTAO_STATION_APPLY_SYNC_EVENT = "CUNTAO_STATION_APPLY_SYNC_EVENT";
 
 }
