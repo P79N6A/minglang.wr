34a18685f7b610800f9e5ffe3c2aa7ae64d3060c
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-05-31 16:46:45 +0800

    zz

---
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/bo/CuntaoFlowRecordBO.java
@@ -0,0 +1,8 @@
+package com.taobao.cun.auge.station.bo;
+
+import com.taobao.cun.auge.dal.domain.CuntaoFlowRecord;
+
+public interface CuntaoFlowRecordBO {
+
+	public void addRecord(CuntaoFlowRecord record);
+}
