bab1d39048a5288224d947eb7291ca9c23b008e2
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-06-02 14:49:23 +0800

    SS

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/bo/impl/PartnerLifecycleBOImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/bo/impl/PartnerLifecycleBOImpl.java
@@ -1,7 +1,5 @@
 package com.taobao.cun.auge.station.bo.impl;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
