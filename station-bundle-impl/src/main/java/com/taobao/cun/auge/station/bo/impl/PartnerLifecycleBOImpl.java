a0585c67ad1b0366dfe652cb1f01046d8390a8c1
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-06-02 15:17:08 +0800

    订单

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/bo/impl/PartnerLifecycleBOImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/bo/impl/PartnerLifecycleBOImpl.java
@@ -3,6 +3,7 @@ package com.taobao.cun.auge.station.bo.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.github.pagehelper.StringUtil;
 import com.taobao.cun.auge.common.utils.DomainUtils;
 import com.taobao.cun.auge.common.utils.ResultUtils;
 import com.taobao.cun.auge.common.utils.ValidateUtils;
@@ -28,7 +29,11 @@ public class PartnerLifecycleBOImpl implements PartnerLifecycleBO {
 		ValidateUtils.notNull(partnerLifecycleDto);
 		
 		PartnerLifecycleItems items = PartnerLifecycleConverter.toPartnerLifecycleItems(partnerLifecycleDto);
-		DomainUtils.beforeInsert(items, DomainUtils.DEFAULT_OPERATOR);
+		if(StringUtil.isEmpty(partnerLifecycleDto.getOperator())){
+			DomainUtils.beforeInsert(items, DomainUtils.DEFAULT_OPERATOR);
+		}else{
+			DomainUtils.beforeInsert(items, partnerLifecycleDto.getOperator());
+		}
 		partnerLifecycleItemsMapper.insert(items);
 	}
 
