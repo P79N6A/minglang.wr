9250e22d101403a7bbec02b1571f0b59e619b52e
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-06-12 10:12:57 +0800

    dd

--- station-bundle-client/src/main/java/com/taobao/cun/auge/station/condition/PartnerInstancePageCondition.java
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/station/condition/PartnerInstancePageCondition.java
@@ -3,33 +3,65 @@ package com.taobao.cun.auge.station.condition;
 import com.taobao.cun.auge.common.Address;
 import com.taobao.cun.auge.common.PageQuery;
 import com.taobao.cun.auge.station.enums.PartnerInstanceStateEnum;
+import com.taobao.cun.auge.station.enums.PartnerLifecycleBondEnum;
+import com.taobao.cun.auge.station.enums.PartnerLifecycleLogisticsApproveEnum;
+import com.taobao.cun.auge.station.enums.PartnerLifecycleQuitProtocolEnum;
+import com.taobao.cun.auge.station.enums.PartnerLifecycleRoleApproveEnum;
+import com.taobao.cun.auge.station.enums.PartnerLifecycleSettledApproveEnum;
+import com.taobao.cun.auge.station.enums.PartnerLifecycleSettledProtocolEnum;
 
 public class PartnerInstancePageCondition extends PageQuery {
 
 	private static final long serialVersionUID = 8658912725579809791L;
-	
+
+	// 村服务站所属村淘组织
 	private Long[] orgIds;
 
+	// 村服务站编号
 	private String stationNum;
 
+	// 村服务站名称
 	private String stationName;
 
-	private String taobaoNick;
-
+	// 村服务站管理员
 	private String managerId;
 
-
+	// 村服务站地址
 	private Address address;
 
-	
+	// 合伙人taobao昵称
+	private String taobaoNick;
+
+	// 合伙人姓名
 	private String partnerName;
-	
+
+	// 合伙人状态
 	private PartnerInstanceStateEnum partnerInstanceState;
-//	private PartnerInstanceTypeEnum partnerType;
-	
+
+	//合伙人类型
+	// private PartnerInstanceTypeEnum partnerType;
 	private String partnerType;
-	
+
+	//所属TP商id
 	private Long providerId;
+
+	//保证金
+	private PartnerLifecycleBondEnum bond;
+
+	//物流审批
+	private PartnerLifecycleLogisticsApproveEnum logisticsApprove;
+
+	//退出协议
+	private PartnerLifecycleQuitProtocolEnum quitProtocol;
+
+	//角色审批
+	private PartnerLifecycleRoleApproveEnum roleApprove;
+
+	//入驻审批
+	private PartnerLifecycleSettledApproveEnum settleApprove;
+
+	//入驻协议
+	private PartnerLifecycleSettledProtocolEnum settleProtocol;
 
 	public String getStationNum() {
 		return stationNum;
@@ -111,12 +143,60 @@ public class PartnerInstancePageCondition extends PageQuery {
 		this.orgIds = orgIds;
 	}
 
-//	public PartnerInstanceTypeEnum getPartnerType() {
-//		return partnerType;
-//	}
-//
-//	public void setPartnerType(PartnerInstanceTypeEnum partnerType) {
-//		this.partnerType = partnerType;
-//	}
+	public PartnerLifecycleBondEnum getBond() {
+		return bond;
+	}
+
+	public void setBond(PartnerLifecycleBondEnum bond) {
+		this.bond = bond;
+	}
+
+	public PartnerLifecycleLogisticsApproveEnum getLogisticsApprove() {
+		return logisticsApprove;
+	}
+
+	public void setLogisticsApprove(PartnerLifecycleLogisticsApproveEnum logisticsApprove) {
+		this.logisticsApprove = logisticsApprove;
+	}
+
+	public PartnerLifecycleQuitProtocolEnum getQuitProtocol() {
+		return quitProtocol;
+	}
+
+	public void setQuitProtocol(PartnerLifecycleQuitProtocolEnum quitProtocol) {
+		this.quitProtocol = quitProtocol;
+	}
+
+	public PartnerLifecycleRoleApproveEnum getRoleApprove() {
+		return roleApprove;
+	}
+
+	public void setRoleApprove(PartnerLifecycleRoleApproveEnum roleApprove) {
+		this.roleApprove = roleApprove;
+	}
+
+	public PartnerLifecycleSettledApproveEnum getSettleApprove() {
+		return settleApprove;
+	}
+
+	public void setSettleApprove(PartnerLifecycleSettledApproveEnum settleApprove) {
+		this.settleApprove = settleApprove;
+	}
+
+	public PartnerLifecycleSettledProtocolEnum getSettleProtocol() {
+		return settleProtocol;
+	}
+
+	public void setSettleProtocol(PartnerLifecycleSettledProtocolEnum settleProtocol) {
+		this.settleProtocol = settleProtocol;
+	}
+
+	// public PartnerInstanceTypeEnum getPartnerType() {
+	// return partnerType;
+	// }
+	//
+	// public void setPartnerType(PartnerInstanceTypeEnum partnerType) {
+	// this.partnerType = partnerType;
+	// }
 
 }
