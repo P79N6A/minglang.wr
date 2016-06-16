b757b510cca41f10a11a96278986d4d6ca1c36b4
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-06-16 17:21:38 +0800

    老客户撒的发反反复复凤飞飞飞凤飞飞凤飞飞凤飞飞

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/notify/listener/ProcessListener.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/notify/listener/ProcessListener.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.fastjson.JSONObject;
+import com.taobao.cun.auge.station.bo.PartnerInstanceBO;
+import com.taobao.cun.auge.station.bo.PartnerLifecycleBO;
+import com.taobao.cun.auge.station.enums.PartnerLifecycleBusinessTypeEnum;
 import com.taobao.cun.auge.station.enums.ProcessApproveResultEnum;
 import com.taobao.cun.auge.station.enums.ProcessBusinessEnum;
 import com.taobao.cun.auge.station.enums.ProcessMsgTypeEnum;
@@ -21,7 +24,13 @@ public class ProcessListener implements MessageListener {
 	private static final Logger logger = LoggerFactory.getLogger(ProcessListener.class);
 
 	@Autowired
-	ProcessApproveResultProcessor processApproveResultProcessor;
+	ProcessProcessor processProcessor;
+
+	@Autowired
+	PartnerInstanceBO partnerInstanceBO;
+
+	@Autowired
+	PartnerLifecycleBO partnerLifecycleBO;
 
 	@Override
 	public void receiveMessage(Message message, MessageStatus status) {
@@ -40,57 +49,61 @@ public class ProcessListener implements MessageListener {
 		logger.info("BpmNotifyRecieveImpl notify:" + ob.toJSONString());
 
 		String msgType = strMessage.getMessageType();
+		String businessCode = ob.getString("businessCode");
+		String objectId = ob.getString("objectId");
 		// 监听流程实例结束
 		if (ProcessMsgTypeEnum.PROC_INST_FINISH.getCode().equals(msgType)) {
-			String businessCode = ob.getString("businessCode");
 			JSONObject instanceStatus = ob.getJSONObject("instanceStatus");
-			String resultCode =instanceStatus.getString("code");
-			String objectId = ob.getString("objectId");
-			String remarks = ob.getString("remarks");
+			String resultCode = instanceStatus.getString("code");
 
 			// 村点强制停业
-			if (ProcessBusinessEnum.stationForcedClosure.getCode().equals(businessCode)
-					|| ProcessBusinessEnum.TPV_FORCED_CLOSURE.getCode().equals(businessCode)) {
+			if (ProcessBusinessEnum.stationForcedClosure.getCode().equals(businessCode)) {
 				processCloseApproveResult(resultCode, objectId);
 				// 村点退出
-			} else if (ProcessBusinessEnum.stationQuitRecord.getCode().equals(businessCode)
-					|| ProcessBusinessEnum.TPV_QUIT.getCode().equals(businessCode)) {
+			} else if (ProcessBusinessEnum.stationQuitRecord.getCode().equals(businessCode)) {
 				processQuitApproveResult(resultCode, objectId);
 			}
 			// 节点被激活
 		} else if (ProcessMsgTypeEnum.ACT_INST_START.getCode().equals(msgType)) {
 			// 任务被激活
 		} else if (ProcessMsgTypeEnum.TASK_ACTIVATED.getCode().equals(msgType)) {
-
+			// 村点强制停业
+			if (ProcessBusinessEnum.stationForcedClosure.getCode().equals(businessCode)) {
+				processProcessor.monitorTaskStarted(Long.valueOf(objectId), PartnerLifecycleBusinessTypeEnum.CLOSING);
+				// 村点退出
+			} else if (ProcessBusinessEnum.stationQuitRecord.getCode().equals(businessCode)) {
+				processProcessor.monitorTaskStarted(Long.valueOf(objectId), PartnerLifecycleBusinessTypeEnum.QUITING);
+			}
 		}
 	}
 
 	/**
 	 * 监听退出审批结果
+	 * 
 	 * @param resultCode
 	 * @param objectId
 	 */
 	private void processQuitApproveResult(String resultCode, String objectId) {
 		try {
 			Long stationApplyId = Long.valueOf(objectId);
-			processApproveResultProcessor.monitorQuitApprove(stationApplyId,
-					ProcessApproveResultEnum.valueof(resultCode));
+			processProcessor.monitorQuitApprove(stationApplyId, ProcessApproveResultEnum.valueof(resultCode));
 		} catch (Exception e) {
-			logger.error("监听审批退出流程失败。stationApplyId = " + objectId);
+			logger.error("监听审批退出流程失败。stationApplyId = " + objectId + " resultCode=" + resultCode, e);
 		}
 	}
 
 	/**
 	 * 监听停业审批结果
+	 * 
 	 * @param resultCode
 	 * @param objectId
 	 */
 	private void processCloseApproveResult(String resultCode, String objectId) {
 		try {
 			Long stationApplyId = Long.valueOf(objectId);
-			processApproveResultProcessor.monitorCloseApprove(stationApplyId, ProcessApproveResultEnum.valueof(resultCode));
+			processProcessor.monitorCloseApprove(stationApplyId, ProcessApproveResultEnum.valueof(resultCode));
 		} catch (Exception e) {
-			logger.error("监听审批停业流程失败。stationApplyId = " + objectId);
+			logger.error("监听审批停业流程失败。stationApplyId = " + objectId + " resultCode=" + resultCode, e);
 		}
 	}
 }
