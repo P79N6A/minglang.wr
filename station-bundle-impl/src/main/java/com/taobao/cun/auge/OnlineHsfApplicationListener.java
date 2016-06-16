f5dc79a799a69bc4be8edc4caa294809f9c0b01f
谨修 <jianke.ljk@alibaba-inc.com>
2016-06-16 10:45:23 +0800

    fix httpclient conflict

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/OnlineHsfApplicationListener.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/OnlineHsfApplicationListener.java
@@ -1,14 +1,13 @@
 package com.taobao.cun.auge;
 
-import java.util.HashMap;
-
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 
 import com.alibaba.springframework.boot.util.env.EnvironmentUtil;
-import com.taobao.cun.auge.common.utils.HttpClientUtil;
 
 /**
  * 日常和本地测试环境自动上线hsf
@@ -25,10 +24,13 @@ public class OnlineHsfApplicationListener implements ApplicationListener<Applica
 	public void onApplicationEvent(ApplicationReadyEvent event) {
 		try {
 			String env = EnvironmentUtil.getEnvironment().name().toLowerCase();
-//			if ("daily".equals(env)) {
-//				String response = HttpClientUtil.get(HSF_ONLINE_URL, new HashMap<String, String>(), null);
-//				System.out.println("－－－－－－－－－－－－online hsf: " + response);
-//			}
+			if ("daily".equals(env)) {
+				HttpClient client =  new HttpClient();  
+				GetMethod method = new GetMethod(HSF_ONLINE_URL);
+				client.executeMethod(method);  
+		        String response = method.getResponseBodyAsString();  				
+				System.out.println("－－－－－－－－－－－－online hsf: " + response);
+			}
 		} catch (Exception e) {
 		}
 	}
