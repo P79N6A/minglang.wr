cd418af9fa00bfd2dd9f6f0d1576f31feb935399
quanzhu.wangqz <quanzhu.wangqz@alibaba-inc.com>
2016-06-17 13:21:18 +0800

    ci

---
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/station/dto/DegradePartnerInstanceSuccessDto.java
@@ -0,0 +1,27 @@
+package com.taobao.cun.auge.station.dto;
+
+import java.io.Serializable;
+
+import com.taobao.cun.auge.common.OperatorDto;
+
+/**
+ *  降级成功dto
+ * @author quanzhu.wangqz
+ *
+ */
+public class DegradePartnerInstanceSuccessDto extends OperatorDto implements Serializable {
+	
+	private static final long serialVersionUID = -902480451682542382L;
+	/**
+	 * 合伙人实例id
+	 */
+	public Long instanceId;
+
+	
+	public Long getInstanceId() {
+		return instanceId;
+	}
+	public void setInstanceId(Long instanceId) {
+		this.instanceId = instanceId;
+	}	
+}
