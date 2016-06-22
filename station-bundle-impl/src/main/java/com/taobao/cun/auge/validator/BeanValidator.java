0063b7ef2dcef40c45ee5c16eecf8510024f5571
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-06-22 13:48:59 +0800

    阿斯蒂芬

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/validator/BeanValidator.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/validator/BeanValidator.java
@@ -9,7 +9,6 @@ import javax.validation.Validator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import com.google.common.collect.Lists;
 
