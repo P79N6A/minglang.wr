0d786beed64202c2b67e5ebb8932ec0db061e845
谨修 <jianke.ljk@alibaba-inc.com>
2016-08-02 16:13:56 +0800

    update

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/PartnerInstanceServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/PartnerInstanceServiceImpl.java
@@ -128,7 +128,7 @@ import com.taobao.hsf.app.spring.util.annotation.HSFProvider;
  *
  */
 @Service("partnerInstanceService")
-@HSFProvider(serviceInterface = PartnerInstanceService.class)
+@HSFProvider(serviceInterface = PartnerInstanceService.class, serviceVersion = "1.0.0.daily.level")
 public class PartnerInstanceServiceImpl implements PartnerInstanceService {
 
 	private static final Logger logger = LoggerFactory.getLogger(PartnerInstanceService.class);
