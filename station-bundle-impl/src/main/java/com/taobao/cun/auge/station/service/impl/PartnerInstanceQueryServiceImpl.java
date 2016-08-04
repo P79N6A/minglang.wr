5228f3f78000a2e8959c6722100857263593c799
谨修 <jianke.ljk@alibaba-inc.com>
2016-08-04 20:06:54 +0800

    yodate

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/PartnerInstanceQueryServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/PartnerInstanceQueryServiceImpl.java
@@ -90,7 +90,7 @@ import com.taobao.security.util.SensitiveDataUtil;
 import com.taobao.util.CalendarUtil;
 
 @Service("partnerInstanceQueryService")
-@HSFProvider(serviceInterface = PartnerInstanceQueryService.class)
+@HSFProvider(serviceInterface = PartnerInstanceQueryService.class, serviceVersion = "1.0.0.daily.level")
 public class PartnerInstanceQueryServiceImpl implements PartnerInstanceQueryService {
 
 	private static final Logger logger = LoggerFactory.getLogger(PartnerInstanceQueryService.class);
