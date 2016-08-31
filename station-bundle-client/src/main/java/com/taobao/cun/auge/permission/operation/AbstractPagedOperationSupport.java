2e787e35bbf368deb1aada5ba593c06dadbf0181
张振环 <zhenhuan.zhangzh@alibaba-inc.com>
2016-08-31 10:40:24 +0800

    ci

---
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/permission/operation/AbstractPagedOperationSupport.java
@@ -0,0 +1,6 @@
+package com.taobao.cun.auge.permission.operation;
+
+public abstract class AbstractPagedOperationSupport extends AbstractOperationSupport implements PagedOperationSupport {
+
+	
+}
--- station-bundle-client/src/main/java/com/taobao/cun/auge/permission/operation/OperationSupport.java
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/permission/operation/OperationSupport.java
@@ -6,4 +6,7 @@ public interface OperationSupport extends OperationAware{
 
 	 Map<String,Object> getDataProperties();
 	 
+	 void setDataProperty(String name,Object value);
+	 
+	 void setDataPropertis(Map<String,Object> properties);
 }
