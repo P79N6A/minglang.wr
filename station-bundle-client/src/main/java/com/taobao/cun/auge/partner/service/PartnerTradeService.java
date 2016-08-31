416d12d543a6dfe63084ea1a11ab3be23ba32e97
haihu.fhh <haihu.fhh@alibaba-inc.com>
2016-08-31 11:24:47 +0800

    人村分离

--- station-bundle-client/src/main/java/com/taobao/cun/auge/partner/service/PartnerTradeService.java
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/partner/service/PartnerTradeService.java
@@ -1,16 +1,13 @@
 package com.taobao.cun.auge.partner.service;
 
-import java.util.Date;
-
 import com.taobao.cun.auge.station.exception.AugeServiceException;
 
 public interface PartnerTradeService {
 	/**
 	 * 校验是否有未结束的订单
 	 * 
-	 * @param buyerId
-	 * @param endDate
+	 * @param instanceId
 	 * @throws AugeServiceException
 	 */
-	public void validateNoEndTradeOrders(Long buyerId, Date endDate) throws AugeServiceException;
+	public void validateNoEndTradeOrders(Long instanceId) throws AugeServiceException;
 }
