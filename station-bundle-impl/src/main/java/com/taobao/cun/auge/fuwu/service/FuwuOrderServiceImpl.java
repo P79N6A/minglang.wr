b313f6be87f88a42cb1cef90eea2944fc002d186
yi.shaoy <yi.shaoy@alibaba-inc.com>
2016-09-22 19:41:20 +0800

    ci code

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/fuwu/service/FuwuOrderServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/fuwu/service/FuwuOrderServiceImpl.java
@@ -101,7 +101,7 @@ public class FuwuOrderServiceImpl implements FuwuOrderService{
 		customer.setSite(OrderItemSiteType.B2BCN.getValue());
 		customer.setUserIp(userIp);
 		customer.setAliId(userId);
-//		customer.setCustomerIdentity(customerIdentity);
+		customer.setCustomerIdentity(customerIdentity);
 		ProductDto product = new ProductDto();
 		product.setMkey(mkey);
 		try {
