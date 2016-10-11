901d800cebc97e32225eb46b8e2b847c791c10d7
jianhui.xujh <jianhui.xujh@alibaba-inc.com>
2016-10-11 16:55:58 +0800

    add new

--- station-bundle-client/src/main/java/com/taobao/cun/auge/station/service/LevelExamDispatchStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.taobao.cun.auge.station.service;
-
-import java.util.Map;
-
-public interface LevelExamDispatchStrategy {
-
-    boolean dispatchExamPaper(Long taoUserId, String nickName, Map<String, Object> context);
-}
