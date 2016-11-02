5127348ff0afe59f60fa515201ff19afea2097ac
quanzhu.wangqz <quanzhu.wangqz@alibaba-inc.com>
2016-11-02 16:26:06 +0800

    ci

---
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/station/dto/FreezeBondDto.java
@@ -0,0 +1,51 @@
+package com.taobao.cun.auge.station.dto;
+
+import javax.validation.constraints.NotNull;
+
+import com.taobao.cun.auge.common.OperatorDto;
+
+/**
+ * 冻结保证金dto
+ * @author quanzhu.wangqz
+ *
+ */
+public class FreezeBondDto extends OperatorDto{
+
+	private static final long serialVersionUID = -8878634406332198903L;
+	/**
+	 * 淘宝userId
+	 */
+	@NotNull(message="taobaoUserId not null")
+	private Long taobaoUserId;
+	
+	/**
+	 * 支付宝账号（支付宝内部id）
+	 */
+	@NotNull(message="accountNo not null")
+	private String accountNo;
+	
+	/**
+	 * 支付宝登录账号
+	 */
+	@NotNull(message="alipayAccount not null")
+	private String alipayAccount;
+	
+	public Long getTaobaoUserId() {
+		return taobaoUserId;
+	}
+	public void setTaobaoUserId(Long taobaoUserId) {
+		this.taobaoUserId = taobaoUserId;
+	}
+	public String getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	public String getAlipayAccount() {
+		return alipayAccount;
+	}
+	public void setAlipayAccount(String alipayAccount) {
+		this.alipayAccount = alipayAccount;
+	}
+}
