a00064f1d79fd4672a3f9affc7f8a66c6907f972
jianhui.xujh <jianhui.xujh@alibaba-inc.com>
2016-11-04 18:16:27 +0800

    修改指标模型

--- station-bundle-client/src/main/java/com/taobao/cun/auge/station/dto/PartnerInstanceLevelGrowthTrendDtoV2.java
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/station/dto/PartnerInstanceLevelGrowthTrendDtoV2.java
@@ -2,96 +2,101 @@ package com.taobao.cun.auge.station.dto;
 
 import java.io.Serializable;
 
+/**
+ * 最近一天各个指标的数据:如最近一天的收入,最近一天的app活跃用户数等 类PartnerInstanceLevelGrowthTrendDtoV2.java的实现描述：趋势数据模型:统计的是最近一天的数据,不是月均数据.
+ * 
+ * @author xujianhui 2016年11月4日 下午2:51:02
+ */
 public class PartnerInstanceLevelGrowthTrendDtoV2 implements Serializable {
 
-	private static final long serialVersionUID = 462091570592784309L;
-	/**
-	 * 日期
-	 */
-	private String statDate;
-	/**
-	 * 日收入
-	 */
-	private Double dailyIncome;
-	/**
-	 * 1.0商品GMV
-	 */
-	private Double dailyGoods1Gmv;
+    private static final long serialVersionUID = 462091570592784309L;
+    /**
+     * 日期
+     */
+    private String            statDate;
+    /**
+     * 最近一天日收入
+     */
+    private Double            lastDayIncome;
+    /**
+     * 最近一天的1.0商品GMV占比
+     */
+    private Double            lastDayGoods1GmvRatio;
 
-	/**
-	 * app收入占比
-	 */
-	private Double dailyAppIncomePercent;
+    /**
+     * 最近一天app活跃用户数
+     */
+    private Double            lastDayActiveAppUserCnt;
 
-	/**
-	 * 购买村全国民数
-	 */
-	private Long dailyBuyVillagerCnt;
+    /**
+     * 最近一天忠实村民数
+     */
+    private Double            lastDayLoyaltyVillagerCnt;
 
-	/**
-	 * 人均购买次数
-	 */
-	private Double dailyAvgBuyTimes;
+    /**
+     * 最近一天复购率
+     */
+    private Double            lastDayRepurchaseRate;
 
-	/**
-	 * 新增app绑定量
-	 */
-	private Long dailyNewAppBindingCnt;
+    /**
+     * 最近一天访问商品数
+     */
+    private Double            lastDayVisitedProductCnt;
 
-	public String getStatDate() {
-		return statDate;
-	}
+    public String getStatDate() {
+        return statDate;
+    }
 
-	public void setStatDate(String statDate) {
-		this.statDate = statDate;
-	}
+    public void setStatDate(String statDate) {
+        this.statDate = statDate;
+    }
 
-	public Double getDailyIncome() {
-		return dailyIncome;
-	}
+    public Double getLastDayIncome() {
+        return lastDayIncome;
+    }
 
-	public void setDailyIncome(Double dailyIncome) {
-		this.dailyIncome = dailyIncome;
-	}
+    public void setLastDayIncome(Double lastDayIncome) {
+        this.lastDayIncome = lastDayIncome;
+    }
 
-	public Double getDailyGoods1Gmv() {
-		return dailyGoods1Gmv;
-	}
+    public Double getLastDayGoods1GmvRatio() {
+        return lastDayGoods1GmvRatio;
+    }
 
-	public void setDailyGoods1Gmv(Double dailyGoods1Gmv) {
-		this.dailyGoods1Gmv = dailyGoods1Gmv;
-	}
+    public void setLastDayGoods1GmvRatio(Double lastDayGoods1GmvRatio) {
+        this.lastDayGoods1GmvRatio = lastDayGoods1GmvRatio;
+    }
 
-	public Double getDailyAppIncomePercent() {
-		return dailyAppIncomePercent;
-	}
+    public Double getLastDayActiveAppUserCnt() {
+        return lastDayActiveAppUserCnt;
+    }
 
-	public void setDailyAppIncomePercent(Double dailyAppIncomePercent) {
-		this.dailyAppIncomePercent = dailyAppIncomePercent;
-	}
+    public void setLastDayActiveAppUserCnt(Double lastDayActiveAppUserCnt) {
+        this.lastDayActiveAppUserCnt = lastDayActiveAppUserCnt;
+    }
 
-	public Long getDailyBuyVillagerCnt() {
-		return dailyBuyVillagerCnt;
-	}
+    public Double getLastDayLoyaltyVillagerCnt() {
+        return lastDayLoyaltyVillagerCnt;
+    }
 
-	public void setDailyBuyVillagerCnt(Long dailyBuyVillagerCnt) {
-		this.dailyBuyVillagerCnt = dailyBuyVillagerCnt;
-	}
+    public void setLastDayLoyaltyVillagerCnt(Double lastDayLoyaltyVillagerCnt) {
+        this.lastDayLoyaltyVillagerCnt = lastDayLoyaltyVillagerCnt;
+    }
 
-	public Double getDailyAvgBuyTimes() {
-		return dailyAvgBuyTimes;
-	}
+    public Double getLastDayRepurchaseRate() {
+        return lastDayRepurchaseRate;
+    }
 
-	public void setDailyAvgBuyTimes(Double dailyAvgBuyTimes) {
-		this.dailyAvgBuyTimes = dailyAvgBuyTimes;
-	}
+    public void setLastDayRepurchaseRate(Double lastDayRepurchaseRate) {
+        this.lastDayRepurchaseRate = lastDayRepurchaseRate;
+    }
 
-	public Long getDailyNewAppBindingCnt() {
-		return dailyNewAppBindingCnt;
-	}
+    public Double getLastDayVisitedProductCnt() {
+        return lastDayVisitedProductCnt;
+    }
 
-	public void setDailyNewAppBindingCnt(Long dailyNewAppBindingCnt) {
-		this.dailyNewAppBindingCnt = dailyNewAppBindingCnt;
-	}
+    public void setLastDayVisitedProductCnt(Double lastDayVisitedProductCnt) {
+        this.lastDayVisitedProductCnt = lastDayVisitedProductCnt;
+    }
 
 }
