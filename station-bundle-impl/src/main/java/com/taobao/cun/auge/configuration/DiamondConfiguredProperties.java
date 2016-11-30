0e8debc196c4a13b6f6b7ab37ecb29cbf85d409f
斗阵 <xiaoxiong.xx@alibaba-inc.com>
2016-11-30 22:31:17 +0800

    diamond

---
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/configuration/DiamondConfiguredProperties.java
@@ -0,0 +1,42 @@
+package com.taobao.cun.auge.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by xiao on 16/11/30.
+ */
+@Component
+@RefreshScope
+public class DiamondConfiguredProperties {
+
+    @Value("${wisdom.audit.apply}")
+    private String apply;
+
+    @Value("${wisdom.audit.pass}")
+    private String pass;
+
+    @Value("${wisdom.audit.fail}")
+    private String fail;
+
+    @Value("${wisdom.audit.mobile}")
+    private String mobile;
+
+    public String getApply() {
+        return apply;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getFail() {
+        return fail;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+}
