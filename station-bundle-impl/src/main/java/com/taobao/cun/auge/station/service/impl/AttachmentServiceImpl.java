80d4b85aa0187e7ecf2307375b94503d92b882a6
斗阵 <xiaoxiong.xx@alibaba-inc.com>
2016-12-22 10:31:05 +0800

    attachment service

---
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/AttachmentServiceImpl.java
@@ -0,0 +1,32 @@
+package com.taobao.cun.auge.station.service.impl;
+
+import com.taobao.cun.auge.common.OperatorDto;
+import com.taobao.cun.auge.station.bo.AttachementBO;
+import com.taobao.cun.auge.station.dto.AttachementDto;
+import com.taobao.cun.auge.station.enums.AttachementBizTypeEnum;
+import com.taobao.cun.auge.station.enums.AttachementTypeIdEnum;
+import com.taobao.cun.auge.station.exception.AugeServiceException;
+import com.taobao.cun.auge.station.service.AttachmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by xiao on 16/12/22.
+ */
+public class AttachmentServiceImpl implements AttachmentService {
+
+    @Autowired
+    private AttachementBO attachementBO;
+
+    @Override
+    public List<AttachementDto> getAttachmentList(Long objectId, AttachementBizTypeEnum bizTypeEnum, AttachementTypeIdEnum attachementTypeId) throws AugeServiceException {
+        return attachementBO.getAttachementList(objectId, bizTypeEnum, attachementTypeId);
+    }
+
+    @Override
+    public void addAttachmentBatch(List<AttachementDto> attachmentDtoList, Long objectId, AttachementBizTypeEnum bizTypeEnum, AttachementTypeIdEnum attachmentTypeId, OperatorDto operatorDto) throws AugeServiceException {
+        attachementBO.addAttachementBatch(attachmentDtoList, objectId, bizTypeEnum, attachmentTypeId, operatorDto);
+    }
+
+}
