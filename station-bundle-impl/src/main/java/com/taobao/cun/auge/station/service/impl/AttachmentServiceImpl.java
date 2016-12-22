241222221cbe979f66304bf8f85178c339ca6342
斗阵 <xiaoxiong.xx@alibaba-inc.com>
2016-12-22 10:37:02 +0800

    attachment service

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/AttachmentServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/AttachmentServiceImpl.java
@@ -7,13 +7,17 @@ import com.taobao.cun.auge.station.enums.AttachementBizTypeEnum;
 import com.taobao.cun.auge.station.enums.AttachementTypeIdEnum;
 import com.taobao.cun.auge.station.exception.AugeServiceException;
 import com.taobao.cun.auge.station.service.AttachmentService;
+import com.taobao.hsf.app.spring.util.annotation.HSFProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by xiao on 16/12/22.
  */
+@Service("attachmentService")
+@HSFProvider(serviceInterface = AttachmentService.class)
 public class AttachmentServiceImpl implements AttachmentService {
 
     @Autowired
