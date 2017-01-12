a1b26ecbfff1d3e6b75d6da1c808925f6109256a
haihu.fhh <haihu.fhh@alibaba-inc.com>
2017-01-12 09:51:13 +0800

    dd

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/configuration/TpaGmvCheckConfiguration.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/configuration/TpaGmvCheckConfiguration.java
@@ -4,20 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
-@Component
-@RefreshScope
 public class TpaGmvCheckConfiguration{
 	
-	@Value("${order.limit.4.auto.close}")
-	private Long orderLimit4AutoClose = 10l;
 
-	public Long getOrderLimit4AutoClose() {
-		return orderLimit4AutoClose;
-	}
-
-	public void setOrderLimit4AutoClose(Long orderLimit4AutoClose) {
-		this.orderLimit4AutoClose = orderLimit4AutoClose;
-	}
-	
 	
 }
