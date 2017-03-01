2780c471910a15c85b840d643926062dd5772cfd
zhenhuan.zhangzh <zhenhuan.zhangzh@alibaba-inc.com>
2017-03-01 13:17:40 +0800

    CI

---
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/testuser/UserTypeTestUserRule.java
@@ -0,0 +1,31 @@
+package com.taobao.cun.auge.testuser;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+
+import com.taobao.cun.auge.dal.domain.PartnerStationRel;
+import com.taobao.cun.auge.station.bo.PartnerInstanceBO;
+
+@Component
+public class UserTypeTestUserRule extends AbstractTestUserRule{
+
+	@Autowired
+	private PartnerInstanceBO partnerInstanceBO;
+	
+	@Override
+	public boolean doCheckTestUser(Long taobaoUserId,String config) {
+		String[] userTypes = StringUtils.commaDelimitedListToStringArray(config);
+		PartnerStationRel partnerInstance = partnerInstanceBO.getActivePartnerInstance(taobaoUserId);
+		Assert.notNull(partnerInstance);
+		return ObjectUtils.containsElement(userTypes, partnerInstance.getType());
+	}
+
+	@Override
+	public String getConfigKey() {
+		return "testUserType";
+	}
+
+}
