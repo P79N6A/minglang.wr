8af38ea59eb586193cb2c1bf38ec7689c99a2bc8
zhenhuan.zhangzh <zhenhuan.zhangzh@alibaba-inc.com>
2017-03-01 18:02:26 +0800

    CI

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/common/MultipleConsumersStreams.java
+++
@@ -1,9 +0,0 @@
-package com.taobao.cun.auge.common;
-
-import org.springframework.cloud.stream.annotation.Input;
-import org.springframework.messaging.SubscribableChannel;
-
-public interface MultipleConsumersStreams {
-	 @Input("c2bquali")
-	 SubscribableChannel inputA();
-}
--- station-bundle-impl/src/main/java/com/taobao/cun/auge/qualification/service/C2BQualificationConsumer.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/qualification/service/C2BQualificationConsumer.java
@@ -6,8 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.messaging.Message;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
@@ -31,11 +30,17 @@ public class C2BQualificationConsumer implements InitializingBean{
 	
 	private static final Logger logger = LoggerFactory.getLogger(C2BQualificationConsumer.class);
 
+	@Value("${qualiTopic}")
+	private String qualiTopic;
+	
+	@Value("${qualiCID}")
+	private String qualiCID;
+	
 	@Autowired
 	SellerQualiServiceAdapter sellerQualiServiceAdapter;
-	 @ServiceActivator(inputChannel = "c2bquali")
-     public void receiveMessage(MessageExt ext) {
+    public void receiveMessage(MessageExt ext) {
 		 try {
+			 logger.info("recevieQualiMessage:["+ext.toString()+"]");
 			QualiLifeCycleMessage qualiLifeCycleMessage = JavaSerializationUtil.deSerialize(ext.getBody());
 			EntityQuali quali = sellerQualiServiceAdapter.queryValidQualiById(qualiLifeCycleMessage.getQid(),qualiLifeCycleMessage.getEidType()).get();
 			Assert.notNull(quali);
@@ -51,47 +56,20 @@ public class C2BQualificationConsumer implements InitializingBean{
      }
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		  /**
-	        * 一个应用创建一个Consumer，由应用来维护此对象，可以设置为全局对象或者单例
-	        * 注意：ConsumerGroupName需要由应用来保证唯一
-	        */
-	        MetaPushConsumer consumer = new MetaPushConsumer("CID_cuntao_quali");
+	        MetaPushConsumer consumer = new MetaPushConsumer(qualiCID);
 	 
-	        /**
-	        * 一个consumer对象可以订阅多个topic 注意：可以通过指定 Tag 的方式来订阅指定 Topic 下的某一种类型的消息 Tag
-	        * 表达式说明: 1. * 表示订阅指定 topic 下的所有消息 2. TagA || TagC || TagD 表示订阅指定 topic
-	        * 下 tags 分别等于 TagA 或 TagC 或 TagD 的消息
-	        */
-	        consumer.subscribe("quali-life-cycle", "*");
+	        consumer.subscribe(qualiTopic, "*");
 	 
 	        consumer.registerMessageListener(new MessageListenerConcurrently() {
-	            /**
-	            * 1、默认msgs里只有一条消息，可以通过设置consumeMessageBatchMaxSize参数来批量接收消息
-	            * 2、如果设置为批量消费方式，要么都成功，要么都失败.
-	            * 3、此方法由MetaQ客户端多个线程回调，需要应用来处理并发安全问题
-	            * 4、抛异常与返回ConsumeConcurrentlyStatus.RECONSUME_LATER等价
-	            * 5、每条消息失败后，会尝试重试，重试5次都失败，则丢弃
-	            */
 	            @Override
 	            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
-	                System.out.println(Thread.currentThread().getName() + " Receive New Messages: " + msgs);
 	                for (MessageExt msg : msgs) {
-	                    /**
-	                    * 对消息进行处理. 1. 如果消息处理成功, 请 return
-	                    * ConsumeConcurrentlyStatus.CONSUME_SUCCESS; 2. 如果消息处理失败, 请
-	                    * return
-	                    * ConsumeConcurrentlyStatus.RECONSUME_LATER,服务端会再次对该消息进行投递;
-	                    */
-	                		 C2BQualificationConsumer.this.receiveMessage(msg);
+	                		C2BQualificationConsumer.this.receiveMessage(msg);
 	                    }
 	                    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
 	                }
 	            }
 	        );
-	 
-	        /**
-	        * Consumer对象在使用之前必须要调用start初始化，初始化一次即可
-	        */
 	        consumer.start();
 	 
 	        System.out.println("Consumer Started.");
