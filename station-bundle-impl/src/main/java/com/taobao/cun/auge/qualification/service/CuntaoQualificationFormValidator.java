ceca79de52a72d02334a5a8979fd0cd35ac1e642
zhenhuan.zhangzh <zhenhuan.zhangzh@alibaba-inc.com>
2017-03-17 13:21:05 +0800

    CI

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/qualification/service/CuntaoQualificationFormValidator.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/qualification/service/CuntaoQualificationFormValidator.java
@@ -8,7 +8,6 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
