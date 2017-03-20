166d758f2620a2655671af212faa86d8eaaa8ba7
yy111026 <yy111026@alibaba-inc.com>
2017-03-20 11:11:49 +0800

    包名修改

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/configuration/HsfConsumer2ndPartyConfiguration.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/configuration/HsfConsumer2ndPartyConfiguration.java
@@ -23,7 +23,7 @@ import com.alibaba.ivy.service.user.TrainingRecordServiceFacade;
 import com.alibaba.ivy.service.user.TrainingTicketServiceFacade;
 import com.alibaba.masterdata.client.service.Employee360Service;
 import com.alibaba.pm.sc.api.quali.SellerQualiService;
-import com.alibaba.pm.sc.portal.api.QualiAccessService;
+import com.alibaba.pm.sc.portal.api.quali.QualiAccessService;
 import com.aliexpress.boot.hsf.HSFGroup;
 import com.aliexpress.boot.hsf.HsfConsumerAutoConfiguration;
 import com.aliexpress.boot.hsf.consumer.HsfConsumerContext;
