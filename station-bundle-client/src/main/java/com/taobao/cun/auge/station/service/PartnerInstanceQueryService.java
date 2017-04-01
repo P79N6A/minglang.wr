c232bb2d78a5e86b50999d5ac5c29a44a19b25bb
quanzhu.wangqz <quanzhu.wangqz@alibaba-inc.com>
2017-04-01 14:48:43 +0800

    根据淘宝useid 查询实例信息

--- station-bundle-client/src/main/java/com/taobao/cun/auge/station/service/PartnerInstanceQueryService.java
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/station/service/PartnerInstanceQueryService.java
@@ -108,6 +108,14 @@ public interface PartnerInstanceQueryService {
 	 * @return
 	 */
 	public List<PartnerInstanceDto> queryByPartnerInstanceIds(List<Long> partnerInstanceIds);
+	
+	/**
+	 * 根据合伙人实例id，批量查询合伙人实例
+	 * 
+	 * @param taobaoUserIds
+	 * @return
+	 */
+	public List<PartnerInstanceDto> queryByTaobaoUserIds(List<Long> taobaoUserIds);
 
 	/**
 	 * 获得状态为活跃[settling,decorating,servicing,closing,closed,quitting(退出待解冻除外)]
