283ffc8eed378938d13aa4f3de921a893dbb9aa8
yy111026 <yy111026@alibaba-inc.com>
2017-04-11 18:52:35 +0800

    错误提示

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/qualification/service/ErrorMessageProperties.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/qualification/service/ErrorMessageProperties.java
@@ -4,11 +4,22 @@ import java.util.Map;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import com.ali.com.google.common.collect.HashBiMap;
+
 @ConfigurationProperties(prefix = "c2berror")
 public class ErrorMessageProperties {
 	
 	private Map<String, String> messages;
 
+	private Map<String, String> fixedErrorMessageCodes;
+			
+	public String getFixedErrorMessageCode(String errorMessage){
+		if (fixedErrorMessageCodes != null) {
+			return HashBiMap.create(fixedErrorMessageCodes).inverse().get(errorMessage);
+		}
+		return null;
+	}
+	
 	public Map<String, String> getMessages() {
 		return messages;
 	}
@@ -24,4 +35,12 @@ public class ErrorMessageProperties {
 		return null;
 	}
 	
+	public Map<String, String> getFixedErrorMessageCodes() {
+		return fixedErrorMessageCodes;
+	}
+
+	public void setFixedErrorMessageCodes(Map<String, String> fixedErrorMessageCodes) {
+		this.fixedErrorMessageCodes = fixedErrorMessageCodes;
+	}
+
 }
