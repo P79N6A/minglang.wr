847632cc0163b630baffdfd4c49cb6eb33706d5b
yi.shaoy <yi.shaoy@alibaba-inc.com>
2017-05-04 17:09:23 +0800

    ci code

---
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/county/impl/StationManagerServiceImpl.java
@@ -0,0 +1,45 @@
+package com.taobao.cun.auge.county.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import com.taobao.cun.auge.county.StationManagerService;
+import com.taobao.cun.auge.county.dto.StationManagerDto;
+import com.taobao.cun.auge.dal.domain.StationManager;
+import com.taobao.cun.auge.dal.domain.StationManagerExample;
+import com.taobao.cun.auge.dal.domain.StationManagerExample.Criteria;
+import com.taobao.cun.auge.dal.mapper.StationManagerMapper;
+import com.taobao.hsf.app.spring.util.annotation.HSFProvider;
+@Service("StationManagerService")
+@HSFProvider(serviceInterface = StationManagerService.class)
+public class StationManagerServiceImpl implements StationManagerService{
+
+	@Autowired
+	StationManagerMapper stationManagerMapper;
+	
+	@Override
+	public List<StationManagerDto> getManagersByStationId(Long stationId) {
+		Assert.notNull(stationId);
+		StationManagerExample e= new StationManagerExample();
+		Criteria c=e.createCriteria();
+		c.andIsDeletedEqualTo("n").andStationIdEqualTo(String.valueOf(stationId));
+		List<StationManager> managers=stationManagerMapper.selectByExample(e);
+		List<StationManagerDto> managerDtos = new ArrayList<StationManagerDto>();
+		for(StationManager manager: managers){
+			StationManagerDto managerDto = new StationManagerDto();
+			managerDto.setExtra(manager.getExtra());
+			managerDto.setId(manager.getId());
+			managerDto.setStationId(manager.getStationId());
+			managerDto.setWorkerId(manager.getWorkerId());
+			managerDto.setWorkerName(manager.getWorkerName());
+			managerDto.setWorkerType(manager.getWorkerType());
+			managerDtos.add(managerDto);
+		}
+		return managerDtos;
+	}
+
+}
