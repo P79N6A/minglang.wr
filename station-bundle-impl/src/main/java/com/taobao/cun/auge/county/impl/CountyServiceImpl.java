79f3e05cf16cc03b36516f68a449bb9220c93eec
haihu.fhh <haihu.fhh@alibaba-inc.com>
2017-05-11 10:22:09 +0800

    55555555555

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/county/impl/CountyServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/county/impl/CountyServiceImpl.java
@@ -19,7 +19,7 @@ import com.taobao.cun.auge.county.dto.CountyStationQueryCondition;
 import com.taobao.cun.auge.station.exception.AugeBusinessException;
 import com.taobao.hsf.app.spring.util.annotation.HSFProvider;
 @Service("CountyService")
-@HSFProvider(serviceInterface = CountyService.class,serviceVersion="1.0.0.daily.fjx")
+@HSFProvider(serviceInterface = CountyService.class)
 public class CountyServiceImpl implements CountyService{
 
 	private static final Logger logger = LoggerFactory.getLogger(CountyServiceImpl.class);
