1a654ebd4cf09b97eb10300088d7e6507563c3c8
斗阵 <xiaoxiong.xx@alibaba-inc.com>
2017-05-19 13:57:43 +0800

    asset mobile service recycle

--- station-bundle-client/src/main/java/com/taobao/cun/auge/asset/dto/AssetDto.java
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/asset/dto/AssetDto.java
@@ -2,10 +2,12 @@ package com.taobao.cun.auge.asset.dto;
 
 import java.io.Serializable;
 
+import com.taobao.cun.auge.client.operator.DefaultOperator;
+
 /**
  * Created by xiao on 17/5/19.
  */
-public class AssetDto extends AssetOperatorDto implements Serializable{
+public class AssetDto extends DefaultOperator implements Serializable{
 
     private static final long serialVersionUID = -757424770912097035L;
 
--- station-bundle-impl/src/main/java/com/taobao/cun/auge/asset/bo/impl/AssetBOImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/asset/bo/impl/AssetBOImpl.java
@@ -583,7 +583,7 @@ public class AssetBOImpl implements AssetBO {
 	@Override
 	public Boolean recycleAsset(AssetDto signDto) {
 		Objects.requireNonNull(signDto.getAliNo(), "编号不能为空");
-		Objects.requireNonNull(signDto.getWorkNo(), "工号不能为空");
+		Objects.requireNonNull(signDto.getOperator(), "操作人不能为空");
 		Objects.requireNonNull(signDto.getOperatorOrgId(), "组织不能为空");
 		AssetExample assetExample = new AssetExample();
 		assetExample.createCriteria().andIsDeletedEqualTo("n").andAliNoEqualTo(signDto.getAliNo()).andStatusEqualTo(AssetStatusEnum.RECYCLE.getCode());
@@ -591,11 +591,11 @@ public class AssetBOImpl implements AssetBO {
 		if (asset == null) {
 			throw new AugeBusinessException("入库失败，该资产不在系统中，请核对资产信息！如有疑问，请联系资产管理员。");
 		}
-		if (!asset.getOwnerWorkno().equals(signDto.getWorkNo()) || !asset.getUseAreaId().equals(signDto.getOperatorOrgId())) {
+		if (!asset.getOwnerWorkno().equals(signDto.getOperator()) || !asset.getUseAreaId().equals(signDto.getOperatorOrgId())) {
 			throw new AugeBusinessException("入库失败，该资产不属于您，请核对资产信息！如有疑问，请联系资产管理员。");
 		}
 		Asset updateAsset = new Asset();
-		DomainUtils.beforeUpdate(updateAsset, signDto.getWorkNo());
+		DomainUtils.beforeUpdate(updateAsset, signDto.getOperator());
 		updateAsset.setStatus(AssetStatusEnum.USE.getCode());
 		updateAsset.setId(asset.getId());
 		return assetMapper.updateByPrimaryKeySelective(updateAsset) > 0;
