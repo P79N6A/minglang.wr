c7cdb29bede6ceff94933dc4e366d7c89689400e
haihu.fhh <haihu.fhh@alibaba-inc.com>
2017-05-31 14:00:57 +0800

    55555555555555

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/CaiNiaoServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/CaiNiaoServiceImpl.java
@@ -17,8 +17,11 @@ import com.taobao.cun.auge.common.OperatorDto;
 import com.taobao.cun.auge.common.utils.DomainUtils;
 import com.taobao.cun.auge.dal.domain.CountyStation;
 import com.taobao.cun.auge.dal.domain.CuntaoCainiaoStationRel;
+import com.taobao.cun.auge.dal.domain.LogisticsStationApply;
+import com.taobao.cun.auge.dal.domain.LogisticsStationApplyExample;
 import com.taobao.cun.auge.dal.domain.Partner;
 import com.taobao.cun.auge.dal.domain.PartnerStationRel;
+import com.taobao.cun.auge.dal.mapper.LogisticsStationApplyMapper;
 import com.taobao.cun.auge.station.adapter.CaiNiaoAdapter;
 import com.taobao.cun.auge.station.bo.CountyStationBO;
 import com.taobao.cun.auge.station.bo.CuntaoCainiaoStationRelBO;
@@ -60,6 +63,8 @@ public class CaiNiaoServiceImpl implements CaiNiaoService {
 	PartnerBO partnerBO;
     @Autowired
     LogisticsStationBO logisticsStationBO;
+    @Autowired
+    LogisticsStationApplyMapper logisticsStationApplyMapper;
 
 	@Transactional(propagation = Propagation.REQUIRED, readOnly = false, rollbackFor = Exception.class)
 	@Override
@@ -596,6 +601,7 @@ public class CaiNiaoServiceImpl implements CaiNiaoService {
 			Long logisId = rel.getLogisticsStationId();
 			if (logisId != null) {
 				logisticsStationBO.changeState(logisId, operatorDto.getOperator(), "QUIT");
+				deleteLogisticsStationApply(logisId, operatorDto.getOperator());
 			}
 			// 删除本地数据菜鸟驿站对应关系
 			cuntaoCainiaoStationRelBO.deleteCuntaoCainiaoStationRel(stationId, CuntaoCainiaoStationRelTypeEnum.STATION);
@@ -606,5 +612,16 @@ public class CaiNiaoServiceImpl implements CaiNiaoService {
 			syncAddCainiaoStationDto.copyOperatorDto(operatorDto);
 			addCainiaoStation(syncAddCainiaoStationDto);
 		}
+	}
+	
+	private void deleteLogisticsStationApply(Long logisticsStationId,String operator){
+		LogisticsStationApply record = new LogisticsStationApply();
+		DomainUtils.beforeDelete(record, operator);
+		
+		LogisticsStationApplyExample example = new LogisticsStationApplyExample();
+		
+		example.createCriteria().andIsDeletedEqualTo("y").andLogisticsStationIdEqualTo(logisticsStationId);
+		
+		logisticsStationApplyMapper.updateByExampleSelective(record, example);
 	}
 }
