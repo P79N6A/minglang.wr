8e61502c5051e5e5b6f62d2fce6869d40dd5196b
chengyu.zhoucy <chengyu.zhoucy@alibaba-inc.com>
2017-06-19 18:34:18 +0800

    ok

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/configuration/BizBaseOrgConfiguration.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/configuration/BizBaseOrgConfiguration.java
@@ -2,12 +2,14 @@ package com.taobao.cun.auge.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.alibaba.springframework.boot.util.builder.HsfConsumerBuilder;
 import com.aliexpress.boot.hsf.HSFGroup;
 import com.taobao.cun.auge.permission.service.EndorUserPermissionService;
 import com.taobao.hsf.app.spring.util.HSFSpringConsumerBean;
 
+@Configuration
 public class BizBaseOrgConfiguration {
 	@Value("${hsf.consumer.org.version}") 
 	private String version;
