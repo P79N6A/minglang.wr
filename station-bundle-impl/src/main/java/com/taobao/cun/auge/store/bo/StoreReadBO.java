4153e3edf04136718bd4de951cd564c463bf40f7
chengyu.zhoucy <chengyu.zhoucy@alibaba-inc.com>
2017-08-19 17:53:23 +0800

    BO

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/store/bo/StoreReadBO.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/store/bo/StoreReadBO.java
@@ -2,6 +2,6 @@ package com.taobao.cun.auge.store.bo;
 
 import com.taobao.cun.auge.store.dto.StoreDto;
 
-public interface StoreReadBo {
+public interface StoreReadBO {
 	StoreDto getStoreDtoByStation(Long stationId);
 }
