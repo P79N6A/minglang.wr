2a1f892f6b4c201ca0e09f3eb4de0be95da972d6
quanzhu.wangqz <quanzhu.wangqz@alibaba-inc.com>
2017-09-11 15:03:19 +0800

    ci

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/asset/bo/impl/AssetSynBOImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/asset/bo/impl/AssetSynBOImpl.java
@@ -107,7 +107,7 @@ public class AssetSynBOImpl implements AssetSynBO {
 	@Autowired
 	private CuntaoOrgServiceClient cuntaoOrgServiceClient;
 	
-	private final static String CREATOR = "sync";
+	private final static String CREATOR = "syncAsset";
 	
 	@Autowired
 	private StationBO stationBO;
