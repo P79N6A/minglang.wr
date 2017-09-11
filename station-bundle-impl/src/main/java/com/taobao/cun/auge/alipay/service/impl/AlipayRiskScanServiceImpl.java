e05111926dde471b968b5a241c4b6de67935937d
haihu.fhh <haihu.fhh@alibaba-inc.com>
2017-09-11 10:00:41 +0800

    迁移支付宝校验服务

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/alipay/service/impl/AlipayRiskScanServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/alipay/service/impl/AlipayRiskScanServiceImpl.java
@@ -22,7 +22,7 @@ import com.taobao.uic.common.domain.BaseUserDO;
 import com.taobao.uic.common.domain.ResultDO;
 import com.taobao.uic.common.service.userinfo.client.UicReadServiceClient;
 
-@Service("alipayRiskScanService")
+@Service("augeAlipayRiskScanService")
 @HSFProvider(serviceInterface= AlipayRiskScanService.class,serviceVersion="1.0.0.daily.fhh")
 public class AlipayRiskScanServiceImpl implements AlipayRiskScanService {
 	private static final Logger logger = LoggerFactory.getLogger(AlipayRiskScanServiceImpl.class);
