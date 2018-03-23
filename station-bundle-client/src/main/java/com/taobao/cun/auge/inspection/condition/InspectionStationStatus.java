82e0c8b9033b50607fc87c01957132c1aede6d0b
zhenhuan.zhangzh <zhenhuan.zhangzh@alibaba-inc.com>
2018-03-23 10:25:39 +0800

    add

--- station-bundle-client/src/main/java/com/taobao/cun/auge/inspection/condition/InspectionStationStatus.java
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/inspection/condition/InspectionStationStatus.java
@@ -4,7 +4,7 @@ public class InspectionStationStatus {
 
 	public static final String SETTLING = "SETTLING"; //入驻中
 	
-	public static final String DECORATING ="DEOCRATING";//装修中
+	public static final String DECORATING ="DECORATING";//装修中
 	
 	public static final String SERVICING = "SERVICING";//服务中
 	
