d3bda92e07a04b68d2500e12ae3b3ca3be487a2a
chengyu.zhoucy <chengyu.zhoucy@alibaba-inc.com>
2018-06-22 12:23:46 +0800

    ok

--- station-bundle-client/src/main/java/com/taobao/cun/auge/station/service/TownBlacknameService.java
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/station/service/TownBlacknameService.java
@@ -1,5 +1,16 @@
 package com.taobao.cun.auge.station.service;
 
+/**
+ *  黑名单镇判断
+ * @author chengyu.zhoucy
+ *
+ */
 public interface TownBlacknameService {
+	/**
+	 * 判断某个县下，某个镇是否属于黑名单
+	 * @param countyName
+	 * @param townName
+	 * @return
+	 */
 	boolean isBlackname(String countyName, String townName);
 }
--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/TownBlacknameServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/TownBlacknameServiceImpl.java
@@ -8,6 +8,11 @@ import com.taobao.cun.auge.dal.mapper.TownBlacknameMapper;
 import com.taobao.cun.auge.station.service.TownBlacknameService;
 import com.taobao.hsf.app.spring.util.annotation.HSFProvider;
 
+/**
+ *  黑名单镇判断
+ * @author chengyu.zhoucy
+ *
+ */
 @Service("townBlacknameService")
 @HSFProvider(serviceInterface = TownBlacknameService.class)
 public class TownBlacknameServiceImpl implements TownBlacknameService {
@@ -17,7 +22,9 @@ public class TownBlacknameServiceImpl implements TownBlacknameService {
 	@Override
 	public boolean isBlackname(String countyName, String townName) {
 		//如果是街道，则属于黑名单之列
-		if(townName.endsWith("街道")) return true;
+		if(townName.endsWith("街道")) {
+			return true;
+		}
 		//检查是否在黑名单中
 		return townBlacknameMapper.countBlackname(countyName, townName) > 0;
 	}
