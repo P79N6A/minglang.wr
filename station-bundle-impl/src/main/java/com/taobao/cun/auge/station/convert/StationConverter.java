f0d9b0746e177a754fbae0e7963a4c347418fd13
zhenhuan.zhangzh <zhenhuan.zhangzh@alibaba-inc.com>
2018-06-25 17:38:58 +0800

    add

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/convert/StationConverter.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/convert/StationConverter.java
@@ -69,6 +69,7 @@ public class StationConverter {
 		stationDto.setFeature(FeatureUtil.toMap(station.getFeature()));
 		stationDto.setPartnerInstanceIsOnTown(PartnerInstanceIsOnTown.valueof(station.getIsOnTown()));
 		stationDto.setTaobaoUserId(station.getTaobaoUserId());
+		stationDto.setCategory(station.getCategory());
 		return stationDto;
 	}
 
