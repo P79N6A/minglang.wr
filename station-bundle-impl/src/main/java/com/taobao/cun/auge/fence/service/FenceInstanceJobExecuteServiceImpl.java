76def21eeb46a2278e345bab72524b9c818d06ae
chengyu.zhoucy <chengyu.zhoucy@alibaba-inc.com>
2018-07-05 08:42:47 +0800

    任务

---
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/fence/service/FenceInstanceJobExecuteServiceImpl.java
@@ -0,0 +1,18 @@
+package com.taobao.cun.auge.fence.service;
+
+import javax.annotation.Resource;
+
+import com.taobao.cun.auge.fence.job.FenceInstanceJobExecutorFacade;
+import com.taobao.hsf.app.spring.util.annotation.HSFProvider;
+
+@HSFProvider(serviceInterface = FenceInstanceJobExecuteService.class)
+public class FenceInstanceJobExecuteServiceImpl implements FenceInstanceJobExecuteService {
+	@Resource
+	private FenceInstanceJobExecutorFacade fenceInstanceJobExecutorFacade;
+	
+	@Override
+	public void execute() {
+		fenceInstanceJobExecutorFacade.startJob();
+	}
+
+}
