a84a426a594b2c4ea3913bf74efddc53d901b32e
斗阵 <xiaoxiong.xx@alibaba-inc.com>
2018-07-09 17:14:45 +0800

    fence template

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/fence/constant/FenceConstants.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/fence/constant/FenceConstants.java
@@ -5,8 +5,19 @@ package com.taobao.cun.auge.fence.constant;
  */
 public interface FenceConstants {
 
+    //围栏状态_启用
     String ENABLE = "ENABLE";
 
+    //围栏状态_禁用
     String DISABLE = "DISABLE";
+
+    //围栏范围_距离
+    String RANGE_DISTANCE = "distance";
+
+    //围栏范围_文本匹配
+    String RANGE_MATCH = "match";
+
+    //围栏范围_行政区划
+    String RANGE_DIVISION = "division";
 
 }
