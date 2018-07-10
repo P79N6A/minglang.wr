9d8f5241639043390f2104fa4fdcef5e3d3b3af7
chengyu.zhoucy <chengyu.zhoucy@alibaba-inc.com>
2018-07-10 14:08:10 +0800

    Fence

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/bo/dto/Test.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/bo/dto/Test.java
@@ -8,7 +8,6 @@ public class Test {
 	public static void main(String[] args) {
 		FenceStationQueryCondition fenceStationQueryCondition = new FenceStationQueryCondition();
 		fenceStationQueryCondition.setFullIdPath("/1/500001/169/700054");
-		fenceStationQueryCondition.setStationLevels(Lists.newArrayList("S5", "S6"));
 		fenceStationQueryCondition.setStationTypes(Lists.newArrayList("TP"));
 		fenceStationQueryCondition.setStationStatus(Lists.newArrayList("SERVICING"));
 		System.out.println(JSON.toJSONString(fenceStationQueryCondition));
