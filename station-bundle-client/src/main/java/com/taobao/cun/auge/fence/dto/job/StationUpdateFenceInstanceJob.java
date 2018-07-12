b7dd4fefe3d0621b990d9d56b9a6a2876f59b288
chengyu.zhoucy <chengyu.zhoucy@alibaba-inc.com>
2018-07-12 15:20:21 +0800

    dddddd

--- station-bundle-client/src/main/java/com/taobao/cun/auge/fence/dto/job/StationUpdateFenceInstanceJob.java
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/fence/dto/job/StationUpdateFenceInstanceJob.java
@@ -1,7 +1,5 @@
 package com.taobao.cun.auge.fence.dto.job;
 
-import java.util.List;
-
 import javax.validation.constraints.NotNull;
 
 /**
@@ -14,21 +12,11 @@ public class StationUpdateFenceInstanceJob extends FenceInstanceJob{
 	@NotNull(message="站点ID不能为空")
 	private Long stationId;
 	
-	private List<Long> templateIds;
-	
 	public Long getStationId() {
 		return stationId;
 	}
 
 	public void setStationId(Long stationId) {
 		this.stationId = stationId;
-	}
-
-	public List<Long> getTemplateIds() {
-		return templateIds;
-	}
-
-	public void setTemplateIds(List<Long> templateIds) {
-		this.templateIds = templateIds;
 	}
 }
