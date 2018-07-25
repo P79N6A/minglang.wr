1fed5ddbd37dd5c4946960ed2cf1d97aac0cb0ed
chengyu.zhoucy <chengyu.zhoucy@alibaba-inc.com>
2018-07-25 13:38:39 +0800

    ok

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/Application.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.taobao.cun.diamond.AugeDiamondListener;
@@ -15,7 +14,6 @@ import com.taobao.hsf.app.spring.util.annotation.EnableHSF;
 @EnableHSF
 @EnableTransactionManagement
 @EnableAspectJAutoProxy
-@EnableAsync
 @ImportResource({"classpath*:application/application-context.xml","classpath*:applicationContext-statemachine.xml"})
 @Import(AugeDiamondListener.class)
 public class Application{
--- station-bundle-impl/src/main/java/com/taobao/cun/auge/fence/service/FenceInstanceJobExecuteServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/fence/service/FenceInstanceJobExecuteServiceImpl.java
@@ -2,8 +2,6 @@ package com.taobao.cun.auge.fence.service;
 
 import javax.annotation.Resource;
 
-import org.springframework.scheduling.annotation.Async;
-
 import com.taobao.cun.auge.fence.job.FenceInstanceJobExecutorFacade;
 import com.taobao.hsf.app.spring.util.annotation.HSFProvider;
 
@@ -13,9 +11,12 @@ public class FenceInstanceJobExecuteServiceImpl implements FenceInstanceJobExecu
 	private FenceInstanceJobExecutorFacade fenceInstanceJobExecutorFacade;
 	
 	@Override
-	@Async
 	public void execute() {
-		fenceInstanceJobExecutorFacade.startJob();
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				fenceInstanceJobExecutorFacade.startJob();
+			}}).run();
 	}
 
 }
