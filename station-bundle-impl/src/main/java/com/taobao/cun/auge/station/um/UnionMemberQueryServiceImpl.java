8af7d13b8bfcd34febd9d1d6a46feb93c94d0625
巧凡 <haihu.fhh@alibaba-inc.com>
2018-09-18 10:20:13 +0800

    全城海报之优盟入驻

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/um/UnionMemberQueryServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/um/UnionMemberQueryServiceImpl.java
@@ -3,6 +3,7 @@ package com.taobao.cun.auge.station.um;
 import com.taobao.cun.auge.common.OperatorDto;
 import com.taobao.cun.auge.common.PageDto;
 import com.taobao.cun.auge.station.condition.UnionMemberPageCondition;
+import com.taobao.cun.auge.station.service.PartnerInstanceQueryService;
 import com.taobao.cun.auge.station.um.dto.UnionMemberDto;
 import com.taobao.hsf.app.spring.util.annotation.HSFProvider;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,8 @@ import org.springframework.stereotype.Service;
 @Service("unionMemberQueryService")
 @HSFProvider(serviceInterface = UnionMemberQueryService.class, clientTimeout = 7000)
 public class UnionMemberQueryServiceImpl implements UnionMemberQueryService {
+
+    private PartnerInstanceQueryService partnerInstanceQueryService;
 
     @Override
     public UnionMemberDto getUnionMember(Long stationId, OperatorDto operatorDto) {
