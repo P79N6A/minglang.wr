a4f19327d0a648b82983d141147d9d14c006fc1e
巧凡 <haihu.fhh@alibaba-inc.com>
2018-09-19 20:05:48 +0800

    全城海报之优盟入驻

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/lifecycle/validator/UmLifeCycleValidator.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/lifecycle/validator/UmLifeCycleValidator.java
@@ -15,6 +15,9 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author haihu.fhh
+ */
 @Component
 public class UmLifeCycleValidator {
 
