5ff7298e9c49a04faa0ff6243a6216fe2f612fce
chengyu.zhoucy <chengyu.zhoucy@alibaba-inc.com>
2018-10-02 18:29:50 +0800

    ok

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/user/service/impl/CuntaoUserOrgServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/user/service/impl/CuntaoUserOrgServiceImpl.java
@@ -431,6 +431,18 @@ public class CuntaoUserOrgServiceImpl implements CuntaoUserOrgService{
 			return "PROVINCE_LEADER";
 		}
 		
+		if(userRole.equals(UserRoleEnum.EXT_COUNTY_LEADER.getCode())) {
+			return "EXT_COUNTY_LEADER";
+		}
+		
+		if(userRole.equals(UserRoleEnum.EXT_TEAM_LEADER.getCode())) {
+			return "EXT_TEAM_LEADER";
+		}
+		
+		if(userRole.equals(UserRoleEnum.EXT_PROVINCE_LEADER.getCode())) {
+			return "EXT_PROVINCE_LEADER";
+		}
+		
 		return "COUNTY_ADMIN";
 	}
 	
