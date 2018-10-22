6e6f34031fe02aa0b5dc9893deea0e22ce49c190
chengyu.zhoucy <chengyu.zhoucy@alibaba-inc.com>
2018-10-22 09:56:38 +0800

    log

---
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/log/service/impl/AppBizLogServiceImpl.java
@@ -0,0 +1,26 @@
+package com.taobao.cun.auge.log.service.impl;
+
+import javax.annotation.Resource;
+
+import com.taobao.cun.auge.log.ExtAppBizLog;
+import com.taobao.cun.auge.log.SimpleAppBizLog;
+import com.taobao.cun.auge.log.bo.AppBizLogBo;
+import com.taobao.cun.auge.log.service.AppBizLogService;
+import com.taobao.hsf.app.spring.util.annotation.HSFProvider;
+
+@HSFProvider(serviceInterface = AppBizLogService.class)
+public class AppBizLogServiceImpl implements AppBizLogService {
+	@Resource
+	private AppBizLogBo appBizLogBo;
+
+	@Override
+	public void addLog(SimpleAppBizLog simpleAppBizLog) {
+		appBizLogBo.addLog(simpleAppBizLog);
+	}
+
+	@Override
+	public void addLog(ExtAppBizLog extAppBizLog) {
+		appBizLogBo.addLog(extAppBizLog);
+	}
+
+}
