2c29f0c1474319cfa03c0cae8fee7bfad09131df
quanzhu.wangqz <quanzhu.wangqz@alibaba-inc.com>
2018-10-23 16:30:42 +0800

    ci

---
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/company/dto/CuntaoEmployeeInfoDto.java
@@ -0,0 +1,119 @@
+package com.taobao.cun.auge.company.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import com.taobao.cun.auge.company.enums.CuntaoEmployeeIdentifierEnum;
+
+/**
+ * 员工信息
+ * 
+ * @author quanzhu.wangqz
+ *
+ */
+public class CuntaoEmployeeInfoDto implements Serializable {
+
+	
+	private static final long serialVersionUID = 530323630204727909L;
+	/** 村淘org工人ID */
+	private Long employeeId;
+	/** taobao nick */
+	private String taobaoNick;
+	/** taobao user id */
+	private Long taobaoUserId;
+	/** 支付宝账号 */
+	private String alipayAccount;
+	/** 姓名 */
+	private String name;
+	/** 身份证号 */
+	private String certNo;
+	/** 手机号 */
+	private String mobile;
+	/** 入职期间 */
+	public Date startJobTime;
+	/** 离职时间 */
+	public Date leaveJobTime;
+	/** 工人身份 */
+	private List<CuntaoEmployeeIdentifierEnum> Identifier;
+
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getTaobaoNick() {
+		return taobaoNick;
+	}
+
+	public void setTaobaoNick(String taobaoNick) {
+		this.taobaoNick = taobaoNick;
+	}
+
+	public Long getTaobaoUserId() {
+		return taobaoUserId;
+	}
+
+	public void setTaobaoUserId(Long taobaoUserId) {
+		this.taobaoUserId = taobaoUserId;
+	}
+
+	public String getAlipayAccount() {
+		return alipayAccount;
+	}
+
+	public void setAlipayAccount(String alipayAccount) {
+		this.alipayAccount = alipayAccount;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCertNo() {
+		return certNo;
+	}
+
+	public void setCertNo(String certNo) {
+		this.certNo = certNo;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public Date getStartJobTime() {
+		return startJobTime;
+	}
+
+	public void setStartJobTime(Date startJobTime) {
+		this.startJobTime = startJobTime;
+	}
+
+	public Date getLeaveJobTime() {
+		return leaveJobTime;
+	}
+
+	public void setLeaveJobTime(Date leaveJobTime) {
+		this.leaveJobTime = leaveJobTime;
+	}
+
+	public List<CuntaoEmployeeIdentifierEnum> getIdentifier() {
+		return Identifier;
+	}
+
+	public void setIdentifier(List<CuntaoEmployeeIdentifierEnum> identifier) {
+		Identifier = identifier;
+	}
+}
--- station-bundle-impl/src/main/java/com/taobao/cun/auge/company/EmployeeReadServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/company/EmployeeReadServiceImpl.java
@@ -1,7 +1,18 @@
 package com.taobao.cun.auge.company;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -11,26 +22,31 @@ import com.taobao.cun.auge.common.result.Result;
 import com.taobao.cun.auge.common.utils.PageDtoUtil;
 import com.taobao.cun.auge.company.dto.CuntaoEmployeeDto;
 import com.taobao.cun.auge.company.dto.CuntaoEmployeeIdentifier;
+import com.taobao.cun.auge.company.dto.CuntaoEmployeeInfoDto;
 import com.taobao.cun.auge.company.dto.CuntaoEmployeeType;
 import com.taobao.cun.auge.company.dto.EmployeeQueryPageCondition;
+import com.taobao.cun.auge.company.enums.CuntaoEmployeeIdentifierEnum;
 import com.taobao.cun.auge.dal.domain.CuntaoEmployee;
 import com.taobao.cun.auge.dal.domain.CuntaoEmployeeExample;
 import com.taobao.cun.auge.dal.domain.CuntaoEmployeeExample.Criteria;
 import com.taobao.cun.auge.dal.domain.CuntaoEmployeeRel;
 import com.taobao.cun.auge.dal.domain.CuntaoEmployeeRelExample;
+import com.taobao.cun.auge.dal.domain.CuntaoServiceVendor;
+import com.taobao.cun.auge.dal.domain.CuntaoServiceVendorExample;
 import com.taobao.cun.auge.dal.mapper.CuntaoEmployeeMapper;
 import com.taobao.cun.auge.dal.mapper.CuntaoEmployeeRelMapper;
+import com.taobao.cun.auge.dal.mapper.CuntaoServiceVendorMapper;
 import com.taobao.cun.auge.failure.AugeErrorCodes;
 import com.taobao.cun.endor.base.client.EndorApiClient;
 import com.taobao.cun.endor.base.dto.UserRoleDto;
 import com.taobao.hsf.app.spring.util.annotation.HSFProvider;
+import com.taobao.uic.common.domain.BasePaymentAccountDO;
+import com.taobao.uic.common.domain.BaseUserDO;
+import com.taobao.uic.common.domain.ResultDO;
+import com.taobao.uic.common.service.userinfo.client.UicPaymentAccountReadServiceClient;
+import com.taobao.uic.common.service.userinfo.client.UicReadServiceClient;
+
 import jersey.repackaged.com.google.common.collect.Lists;
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 
 @Service("employeeReadServiceImpl")
 @HSFProvider(serviceInterface = EmployeeReadService.class)
@@ -46,6 +62,15 @@ public class EmployeeReadServiceImpl implements EmployeeReadService{
 	@Autowired
 	@Qualifier("storeEndorApiClient")
 	private EndorApiClient storeEndorApiClient;
+	
+	@Autowired
+	private UicPaymentAccountReadServiceClient uicPaymentAccountReadServiceClient;
+
+	@Autowired
+	private UicReadServiceClient uicReadServiceClient;
+	
+	@Autowired
+	private CuntaoServiceVendorMapper cuntaoServiceVendorMapper;
 	
 	private static final Logger logger = LoggerFactory.getLogger(EmployeeReadServiceImpl.class);
 
@@ -198,6 +223,69 @@ public class EmployeeReadServiceImpl implements EmployeeReadService{
 			ErrorInfo errorInfo = ErrorInfo.of(AugeErrorCodes.SYSTEM_ERROR_CODE, null, "系统异常");
 			return Result.of(errorInfo);
 		}
+	}
+	
+	@Override
+	public Result<List<CuntaoEmployeeInfoDto>> queryVendorEmployeeByTaobaoUserIdForBx(Long taobaoUserId) {
+		List<CuntaoEmployeeInfoDto> result = new ArrayList<CuntaoEmployeeInfoDto>();
+		//根据村小二taobaoUserId取服务商
+		CuntaoServiceVendorExample  example = new CuntaoServiceVendorExample();
+		example.createCriteria().andIsDeletedEqualTo("n").andTaobaoUserIdEqualTo(taobaoUserId);
+		List<CuntaoServiceVendor> vendors = cuntaoServiceVendorMapper.selectByExample(example);
+		
+		if(CollectionUtils.isEmpty(vendors)){
+			return Result.of(result);
+		}
+		//根据服务商ID取工人id和身份(送货员，安装员)，is_deleted=‘y’的也要取
+		List<String> identifiers = new ArrayList<String>();
+		identifiers.add(CuntaoEmployeeIdentifierEnum.VENDOR_DISTRIBUTOR.getCode());
+		identifiers.add(CuntaoEmployeeIdentifierEnum.VENDOR_INSTALLER.getCode());
+		CuntaoEmployeeRelExample cuntaoVendorEmployeeExample = new CuntaoEmployeeRelExample();
+		cuntaoVendorEmployeeExample.createCriteria().andOwnerIdEqualTo(vendors.get(0).getId()).andTypeEqualTo(CuntaoEmployeeType.vendor.name()).andIdentifierIn(identifiers);
+		List<CuntaoEmployeeRel> reLs = cuntaoEmployeeRelMapper.selectByExample(cuntaoVendorEmployeeExample);
+		List<Long> employeeIds = reLs.stream().map(EmployeeRel -> EmployeeRel.getEmployeeId()).collect(Collectors.toList());
+		//根据工人ID，取工人基本信息
+		if (CollectionUtils.isNotEmpty(employeeIds)) {
+			CuntaoEmployeeExample cuntaoEmployeeExample = new CuntaoEmployeeExample();
+			cuntaoEmployeeExample.createCriteria().andTypeEqualTo(CuntaoEmployeeType.vendor.name()).andIdIn(employeeIds);
+			List<CuntaoEmployee> employees = cuntaoEmployeeMapper.selectByExample(cuntaoEmployeeExample);
+			if(CollectionUtils.isNotEmpty(employees)){
+				for(CuntaoEmployee e : employees){
+					CuntaoEmployeeInfoDto dto = new CuntaoEmployeeInfoDto();
+					ResultDO<BasePaymentAccountDO> basePaymentAccountDOResult = uicPaymentAccountReadServiceClient.getAccountByUserId(e.getTaobaoUserId());
+					dto.setAlipayAccount(basePaymentAccountDOResult.getModule().getOutUser());
+			        ResultDO<BaseUserDO> baseUserDOresult = uicReadServiceClient.getBaseUserByUserId(taobaoUserId);
+			        BaseUserDO baseUserDO = baseUserDOresult.getModule();
+					dto.setCertNo(baseUserDO.getIdCardNumber());
+					dto.setEmployeeId(e.getId());
+					if ("y".equals(e.getIsDeleted())) {
+						dto.setLeaveJobTime(e.getGmtModified());
+					}
+					
+					dto.setMobile(e.getMobile());
+					dto.setName(e.getName());
+					dto.setStartJobTime(e.getGmtCreate());
+					dto.setTaobaoNick(e.getTaobaoNick());
+					dto.setIdentifier(buildIdentifier(e.getId(),reLs));
+					dto.setTaobaoUserId(e.getTaobaoUserId());
+					result.add(dto);
+				}
+			}
+		}
+		return Result.of(result);
+	}
+	
+	private static List<CuntaoEmployeeIdentifierEnum> buildIdentifier(Long employeeId,List<CuntaoEmployeeRel> reLs){
+		if (CollectionUtils.isNotEmpty(reLs)) {
+			Set<String> s = new HashSet<String>();
+			for(CuntaoEmployeeRel e : reLs){
+				if (employeeId.equals(e.getEmployeeId())) {
+					s.add(e.getIdentifier());
+				}
+			}
+			return s.stream().map(employee -> CuntaoEmployeeIdentifierEnum.valueof(employee)).collect(Collectors.toList());
+		}
+		return null;
 	}
 
 }
