0a57b77b9992da614b28f677565c4bce9865de0c
巧凡 <haihu.fhh@alibaba-inc.com>
2018-10-24 11:06:53 +0800

    新增优盟新增锁，防止重复提交

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/configuration/TairConfiguration.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/configuration/TairConfiguration.java
@@ -3,11 +3,29 @@ package com.taobao.cun.auge.configuration;
 import com.taobao.cun.auge.lock.ManualReleaseDistributeLock;
 import com.taobao.cun.auge.lock.TairManualReleaseDistributeLock;
 import com.taobao.cun.crius.tair.RdbTairFacade;
+import com.taobao.cun.crius.tair.impl.LdbTairFacadeImpl;
+import com.taobao.cun.crius.tair.impl.MdbTairFacadeImpl;
+import com.taobao.cun.crius.tair.impl.RdbTairFacadeImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TairConfiguration {
+
+	@Bean
+	public MdbTairFacadeImpl mdbTairFacade() {
+		return new MdbTairFacadeImpl();
+	}
+
+	@Bean
+	public RdbTairFacadeImpl rdbTairFacade() throws Exception {
+		return new RdbTairFacadeImpl();
+	}
+
+	@Bean
+	public LdbTairFacadeImpl ldbTairFacade() {
+		return new LdbTairFacadeImpl();
+	}
 
 	@Bean
 	public ManualReleaseDistributeLock manualReleaseDistributeLock(RdbTairFacade rdbTairFacade) throws Exception {
