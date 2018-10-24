ae1351af416bee5d9e44f9cec8392121e1b7186e
巧凡 <haihu.fhh@alibaba-inc.com>
2018-10-24 16:06:05 +0800

    新增优盟新增锁，防止重复提交

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/um/UnionMemberServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/um/UnionMemberServiceImpl.java
@@ -133,7 +133,7 @@ public class UnionMemberServiceImpl implements UnionMemberService {
         BeanValidator.validateWithThrowable(addDto);
         String taobaoNick = addDto.getTaobaoNick();
         try {
-            boolean lockResult = distributeLock.lock("unionMember-addUnionMember", taobaoNick, 3);
+            boolean lockResult = distributeLock.lock("unionMember-addUnionMember", taobaoNick, 100);
             if (!lockResult) {
                 logger.error("distributeLock failed: {}", JSON.toJSONString(addDto));
                 throw new AugeSystemException("请勿重复提交");
@@ -197,7 +197,7 @@ public class UnionMemberServiceImpl implements UnionMemberService {
             logger.error(JSON.toJSONString(addDto), e);
             throw new AugeSystemException(AugeErrorCodes.ILLEGAL_EXT_RESULT_ERROR_CODE, "系统异常");
         }finally {
-            distributeLock.unlock("unionMember-addUnionMember", taobaoNick);
+            //distributeLock.unlock("unionMember-addUnionMember", taobaoNick);
         }
     }
 
