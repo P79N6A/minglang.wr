24411f8ae2dc889bab14bdd17b59effca602d809
斗阵 <xiaoxiong.xx@alibaba-inc.com>
2018-11-05 16:57:06 +0800

    station trans check

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/check/impl/CheckerFactory.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/check/impl/CheckerFactory.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
