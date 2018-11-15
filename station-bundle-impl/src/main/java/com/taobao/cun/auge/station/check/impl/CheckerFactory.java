95fb0cf384ec33c16fbfe0cb16c608840421430d
斗阵 <xiaoxiong.xx@alibaba-inc.com>
2018-11-15 11:35:54 +0800

    station trans check

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/check/impl/CheckerFactory.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/check/impl/CheckerFactory.java
@@ -2,6 +2,7 @@ package com.taobao.cun.auge.station.check.impl;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.BeansException;
@@ -18,17 +19,27 @@ public class CheckerFactory implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
 
+    private static final Map<Class, List<Object>> MAP = new HashMap<>(2);
+
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         CheckerFactory.applicationContext = applicationContext;
     }
 
     public static <T> List<T> getCheckerList(Class<T> type) {
-        Map<String, T> beansOfType = applicationContext.getBeansOfType(type);
-        if (!CollectionUtils.isEmpty(beansOfType)) {
-            return new ArrayList<>(beansOfType.values());
+        if (MAP.get(type) == null) {
+            synchronized (CheckerFactory.class) {
+                if (MAP.get(type) == null) {
+                    Map<String, T> beansOfType = applicationContext.getBeansOfType(type);
+                    if (CollectionUtils.isEmpty(beansOfType)) {
+                        MAP.put(type, Collections.emptyList());
+                    } else {
+                        MAP.put(type, new ArrayList<>(beansOfType.values()));
+                    }
+                }
+            }
         }
-        return Collections.emptyList();
+        return (List<T>)MAP.get(type);
     }
 
 }
