175a2ba16a9712abffed3e5bc7733c3e3df3ce14
quanzhu.wangqz <quanzhu.wangqz@alibaba-inc.com>
2018-12-03 19:23:03 +0800

    ci

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/asset/bo/impl/AssetCheckInfoBOImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/asset/bo/impl/AssetCheckInfoBOImpl.java
@@ -1,33 +1,83 @@
 package com.taobao.cun.auge.asset.bo.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.alibaba.fastjson.JSONObject;
 import com.taobao.cun.auge.asset.bo.AssetCheckInfoBO;
 import com.taobao.cun.auge.asset.dto.AssetCheckInfoAddDto;
 import com.taobao.cun.auge.asset.dto.AssetCheckInfoCondition;
 import com.taobao.cun.auge.asset.dto.AssetCheckInfoDto;
+import com.taobao.cun.auge.asset.dto.CountyCheckCountDto;
+import com.taobao.cun.auge.asset.dto.CountyFollowCheckCountDto;
+import com.taobao.cun.auge.asset.enums.AssetCheckInfoCategoryTypeEnum;
 import com.taobao.cun.auge.common.OperatorDto;
 import com.taobao.cun.auge.common.PageDto;
+import com.taobao.cun.auge.common.utils.DomainUtils;
+import com.taobao.cun.auge.dal.domain.AssetCheckInfo;
+import com.taobao.cun.auge.dal.mapper.AssetCheckInfoMapper;
+import com.taobao.cun.auge.failure.AugeErrorCodes;
+import com.taobao.cun.auge.station.enums.OperatorTypeEnum;
+import com.taobao.cun.auge.station.exception.AugeBusinessException;
 
 @Component
 public class AssetCheckInfoBOImpl implements AssetCheckInfoBO {
-
+	
+	@Autowired
+	private AssetCheckInfoMapper assetCheckInfoMapper;
+	
 	@Override
-	public void addCheckInfo(AssetCheckInfoAddDto addDto) {
-		// TODO Auto-generated method stub
+	public Boolean addCheckInfo(AssetCheckInfoAddDto addDto) {
+		Objects.requireNonNull(addDto, "参数不能为空");
+		Objects.requireNonNull(addDto.getSerialNo(), "序列号不能为空");
+		Objects.requireNonNull(addDto.getCheckType(), "盘点类型不能为空");
+		if (!OperatorTypeEnum.BUC.getCode().equals(addDto.getOperatorType()) && 
+				!OperatorTypeEnum.HAVANA.getCode().equals(addDto.getOperatorType())){
+			throw new AugeBusinessException(AugeErrorCodes.ASSET_BUSINESS_ERROR_CODE,"盘点人必须是村小二,或者县小二");
+		}
+		AssetCheckInfo record = new AssetCheckInfo();
+		record.setAliNo(addDto.getAliNo());
+		record.setAssetType(assetType);
+		record.setCategory(addDto.getCategoryType());
+		record.setAttFile(JSONObject.toJSONString(addDto.getImages()));
+		record.setCheckType(addDto.getCheckType());
+		record.setSerialNo(addDto.getSerialNo());
+		
+		if(OperatorTypeEnum.BUC.getCode().equals(addDto.getOperatorType())){
+			record.setCheckerAreaId(checkerAreaId);
+			record.setCheckerAreaName(checkerAreaName);
+			record.setCheckerAreaType(checkerAreaType);
+			record.setCheckerId(checkerId);
+			record.setCheckerName(checkerName);
+			record.setCheckTime(checkTime);
+			record.setCountyOrgId(countyOrgId);
+			
+		}
+		DomainUtils.beforeInsert(record, addDto.getOperator());
+		assetCheckInfoMapper.insert(record);
+		return null;
 
+	}
+	
+	private String bulidAssetType(String category){
+		List<String> itList = new ArrayList<String>();
+		itList.add(AssetCheckInfoCategoryTypeEnum.)
 	}
 
 	@Override
-	public void delCheckInfo(Long infoId, OperatorDto operator) {
+	public Boolean delCheckInfo(Long infoId, OperatorDto operator) {
 		// TODO Auto-generated method stub
-
+		return null;
 	}
 
 	@Override
-	public void confrimCheckInfo(Long infoId, OperatorDto operator) {
+	public Boolean confrimCheckInfo(Long infoId, OperatorDto operator) {
 		// TODO Auto-generated method stub
-
+		return null;
 	}
 
 	@Override
@@ -44,6 +94,18 @@ public class AssetCheckInfoBOImpl implements AssetCheckInfoBO {
 
 	@Override
 	public AssetCheckInfoDto getCheckInfoById(Long infoId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CountyCheckCountDto getCountyCheckCount(Long countyOrgId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CountyFollowCheckCountDto getCountyFollowCheckCount(Long countyOrgId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- station-bundle-impl/src/main/java/com/taobao/cun/auge/asset/bo/impl/AssetCheckTaskBOImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/asset/bo/impl/AssetCheckTaskBOImpl.java
@@ -2,26 +2,60 @@ package com.taobao.cun.auge.asset.bo.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.alibaba.fastjson.JSONObject;
 import com.taobao.cun.auge.asset.bo.AssetCheckTaskBO;
 import com.taobao.cun.auge.asset.dto.AssetCheckTaskCondition;
 import com.taobao.cun.auge.asset.dto.AssetCheckTaskDto;
 import com.taobao.cun.auge.asset.dto.FinishTaskForCountyDto;
+import com.taobao.cun.auge.asset.enums.AssetCheckTaskTaskStatusEnum;
+import com.taobao.cun.auge.asset.enums.AssetCheckTaskTaskTypeEnum;
+import com.taobao.cun.auge.asset.enums.AssetUseAreaTypeEnum;
 import com.taobao.cun.auge.common.PageDto;
+import com.taobao.cun.auge.common.utils.DomainUtils;
+import com.taobao.cun.auge.common.utils.ResultUtils;
+import com.taobao.cun.auge.dal.domain.AssetCheckTask;
+import com.taobao.cun.auge.dal.domain.AssetCheckTaskExample;
+import com.taobao.cun.auge.dal.domain.AssetCheckTaskExample.Criteria;
+import com.taobao.cun.auge.dal.mapper.AssetCheckTaskMapper;
+import com.taobao.cun.auge.failure.AugeErrorCodes;
+import com.taobao.cun.auge.station.exception.AugeBusinessException;
 
 @Component
 public class AssetCheckTaskBOImpl implements AssetCheckTaskBO {
-
+	
+	@Autowired
+	private AssetCheckTaskMapper assetCheckTaskMapper;
 	@Override
 	public void initTaskForStation(String taskType, String taskCode, Long taobaoUserId) {
 		// TODO Auto-generated method stub
 
 	}
+	
 
 	@Override
-	public void finishTaskForStation(Long taobaoUserId) {
-		// TODO Auto-generated method stub
+	public Boolean finishTaskForStation(Long taobaoUserId) {
+		
+		AssetCheckTaskExample example = new AssetCheckTaskExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andIsDeletedEqualTo("n");
+		criteria.andCheckerIdEqualTo(String.valueOf(taobaoUserId));
+		criteria.andCheckerTypeEqualTo(AssetUseAreaTypeEnum.STATION.name());
+		criteria.andTaskTypeEqualTo(AssetCheckTaskTaskTypeEnum.STATION_CHECK.getCode());
+		AssetCheckTask at = ResultUtils.selectOne(assetCheckTaskMapper.selectByExample(example));
+		if (at == null) {
+			throw new AugeBusinessException(AugeErrorCodes.ASSET_BUSINESS_ERROR_CODE,"未查询到当前服务站盘点任务");
+		}
+		String status = at.getTaskStatus();
+		at.setTaskStatus(AssetCheckTaskTaskStatusEnum.DONE.getCode());
+		DomainUtils.beforeUpdate(at, String.valueOf(taobaoUserId));
+		assetCheckTaskMapper.updateByPrimaryKeySelective(at);
+		if(!AssetCheckTaskTaskStatusEnum.DONE.getCode().equals(status)) {
+			//TODO:结束运营任务
+		}
+		return Boolean.TRUE;
 
 	}
 
@@ -32,9 +66,29 @@ public class AssetCheckTaskBOImpl implements AssetCheckTaskBO {
 	}
 
 	@Override
-	public void finishTaskForCounty(FinishTaskForCountyDto param) {
-		// TODO Auto-generated method stub
-
+	public Boolean finishTaskForCounty(FinishTaskForCountyDto param) {
+		AssetCheckTaskExample example = new AssetCheckTaskExample();
+		Criteria criteria = example.createCriteria();
+		criteria.andIsDeletedEqualTo("n");
+		criteria.andCheckerIdEqualTo(param.getOperator());
+		criteria.andCheckerTypeEqualTo(AssetUseAreaTypeEnum.COUNTY.name());
+		criteria.andTaskTypeEqualTo(param.getTaskType());
+		AssetCheckTask at = ResultUtils.selectOne(assetCheckTaskMapper.selectByExample(example));
+		
+		if (at == null) {
+			throw new AugeBusinessException(AugeErrorCodes.ASSET_BUSINESS_ERROR_CODE,"未查询到当前县点盘点任务");
+		}
+		String status = at.getTaskStatus();
+		at.setTaskStatus(AssetCheckTaskTaskStatusEnum.DONE.getCode());
+		at.setLostAsset(JSONObject.toJSONString(param.getLostAsset()));
+		at.setWaitBackAsset(JSONObject.toJSONString(param.getWaitBackAsset()));
+		at.setOtherReason(param.getOtherReason());
+		DomainUtils.beforeUpdate(at, param.getOperator());
+		assetCheckTaskMapper.updateByPrimaryKeySelective(at);
+		if(!AssetCheckTaskTaskStatusEnum.DONE.getCode().equals(status)) {
+			//TODO:结束流程任务
+		}
+		return  Boolean.TRUE;
 	}
 
 	@Override
