147b975a4169c439a2fb161750acfe10e16782d0
qingwu.yhp <qingwu.yhp@alibaba-inc.com>
2018-12-17 13:22:50 +0800

    装修反馈接入系统消息

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/configuration/DiamondConfiguredProperties.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/configuration/DiamondConfiguredProperties.java
@@ -124,6 +124,9 @@ public class DiamondConfiguredProperties {
     @Value("${decorate.design.county.audit.activity.id}")
     private String decorateDesignCountyAuditActivityId;
 
+    @Value("${decorate.feedback.route.url}")
+    private String decorateFeedbackRouteUrl;
+
     //供应商Id,村淘自营门店对应uic的userId为3405569954
     @Value("${supplierTbId}")
     private Long supplierTbId;
@@ -172,6 +175,14 @@ public class DiamondConfiguredProperties {
 
     public String getMobile() {
         return mobile;
+    }
+
+    public String getDecorateFeedbackRouteUrl() {
+        return decorateFeedbackRouteUrl;
+    }
+
+    public void setDecorateFeedbackRouteUrl(String decorateFeedbackRouteUrl) {
+        this.decorateFeedbackRouteUrl = decorateFeedbackRouteUrl;
     }
 
     public Map<String, String> getSupplierMap() {
