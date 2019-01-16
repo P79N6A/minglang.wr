1166ebe15df41e7a3dd7d74781e7893722d00429
巧凡 <haihu.fhh@alibaba-inc.com>
2019-01-16 17:45:11 +0800

    站点新用户

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/StationNewCustomerServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/station/service/impl/StationNewCustomerServiceImpl.java
@@ -226,7 +226,7 @@ public class StationNewCustomerServiceImpl implements StationNewCustomerService 
         c.setTime(realInterestTime);
 
         //首登时间+2天
-        c.add(Calendar.DAY_OF_MONTH, c.get(Calendar.DAY_OF_MONTH) + 2);
+        c.add(Calendar.DAY_OF_MONTH, 2);
 
         c.set(Calendar.HOUR_OF_DAY, 0);
         c.set(Calendar.MINUTE, 0);
