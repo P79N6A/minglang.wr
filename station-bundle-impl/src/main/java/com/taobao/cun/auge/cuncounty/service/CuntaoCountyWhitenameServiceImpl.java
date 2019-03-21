9d65c3bfd7e99ed755e90bed6474e3f2914cff67
chengyu.zhoucy <chengyu.zhoucy@alibaba-inc.com>
2019-03-21 11:37:01 +0800

    ok

---
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/cuncounty/service/CuntaoCountyWhitenameServiceImpl.java
@@ -0,0 +1,21 @@
+package com.taobao.cun.auge.cuncounty.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import com.taobao.cun.auge.cuncounty.bo.CuntaoCountyWhitenameBo;
+import com.taobao.cun.auge.cuncounty.dto.CuntaoCountyWhitenameDto;
+import com.taobao.hsf.app.spring.util.annotation.HSFProvider;
+
+@HSFProvider(serviceInterface = CuntaoCountyWhitenameService.class)
+public class CuntaoCountyWhitenameServiceImpl implements CuntaoCountyWhitenameService {
+	@Resource
+	private CuntaoCountyWhitenameBo cuntaoCountyWhitenameBo;
+	
+	@Override
+	public List<CuntaoCountyWhitenameDto> getCuntaoCountyWhitenames() {
+		return cuntaoCountyWhitenameBo.getCuntaoCountyWhitenames();
+	}
+
+}
