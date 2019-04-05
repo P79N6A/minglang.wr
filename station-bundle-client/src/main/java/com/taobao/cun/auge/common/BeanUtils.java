c897c9a831ed1b25f064720b3cef5e2766d9fde6
chengyu.zhoucy <chengyu.zhoucy@alibaba-inc.com>
2019-04-05 22:04:43 +0800

    ok

---
+++ station-bundle-client/src/main/java/com/taobao/cun/auge/common/BeanUtils.java
@@ -0,0 +1,19 @@
+package com.taobao.cun.auge.common;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+public class BeanUtils {
+	public static void copyMap(Map<String, Object> source, Object target) {
+		source.keySet().forEach(k->{
+			if(PropertyUtils.isWriteable(target, k)) {
+				try {
+					PropertyUtils.setProperty(target, k, source.get(k));
+				} catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+				}
+			}
+		});
+	}
+}
