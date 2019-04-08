e01e41ee312d72a28c0e3bd858c06b2aa5bd69db
谨修 <jianke.ljk@alibaba-inc.com>
2019-04-08 14:53:30 +0800

    commit

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/lifecycle/statemachine/StateMachineEvent.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/lifecycle/statemachine/StateMachineEvent.java
@@ -5,14 +5,34 @@ import java.util.stream.Stream;
 import org.springframework.util.Assert;
 
 public enum StateMachineEvent {
-
-    SETTLING_EVENT("SETTLING_EVENT","SETTLING"),
-    DECORATING_EVENT("DECORATING_EVENT","DECORATING"),
-    SERVICING_EVENT("SERVICING_EVENT","SERVICING"),
-    CLOSING_EVENT("CLOSING_EVENT","CLOSING"),
-    CLOSED_EVENT("CLOSED_EVENT","CLOSED"),
-    QUITING_EVENT("QUITING_EVENT","QUITING"),
-    QUIT_EVENT("QUIT_EVENT","QUIT");
+    /**
+     * 入驻中
+     */
+    SETTLING_EVENT("SETTLING_EVENT", "SETTLING"),
+    /**
+     * 装修中
+     */
+    DECORATING_EVENT("DECORATING_EVENT", "DECORATING"),
+    /**
+     * 服务中
+     */
+    SERVICING_EVENT("SERVICING_EVENT", "SERVICING"),
+    /**
+     * 停业申请中
+     */
+    CLOSING_EVENT("CLOSING_EVENT", "CLOSING"),
+    /**
+     * 已停业
+     */
+    CLOSED_EVENT("CLOSED_EVENT", "CLOSED"),
+    /**
+     * 退出申请中
+     */
+    QUITING_EVENT("QUITING_EVENT", "QUITING"),
+    /**
+     * 已退出
+     */
+    QUIT_EVENT("QUIT_EVENT", "QUIT");
 
     private String event;
     private String state;
@@ -33,20 +53,20 @@ public enum StateMachineEvent {
         this.state = state;
     }
 
-    private StateMachineEvent(String event,String state){
+    private StateMachineEvent(String event, String state) {
         this.event = event;
         this.state = state;
     }
 
-    public static StateMachineEvent valueOfState(String state){
+    public static StateMachineEvent valueOfState(String state) {
         StateMachineEvent sme = Stream.of(StateMachineEvent.values()).filter(value -> value.getState().equals(state)).findFirst().orElse(null);
-        Assert.notNull(sme);
+        Assert.notNull(sme, "StateMachineEvent not exist");
         return sme;
     }
 
-    public static StateMachineEvent valueOfEvent(String event){
-        StateMachineEvent sme =  Stream.of(StateMachineEvent.values()).filter(value -> value.getEvent().equals(event)).findFirst().orElse(null);
-        Assert.notNull(sme);
+    public static StateMachineEvent valueOfEvent(String event) {
+        StateMachineEvent sme = Stream.of(StateMachineEvent.values()).filter(value -> value.getEvent().equals(event)).findFirst().orElse(null);
+        Assert.notNull(sme, "StateMachineEvent not exist");
         return sme;
     }
 
