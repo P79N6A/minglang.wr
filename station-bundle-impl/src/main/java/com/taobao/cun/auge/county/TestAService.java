1574d42d61d121f9c8a72ebb9a74baa77de3fb89
chengyu.zhoucy <chengyu.zhoucy@alibaba-inc.com>
2019-04-13 12:02:13 +0800

    ok

---
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/county/TestAService.java
@@ -0,0 +1,9 @@
+package com.taobao.cun.auge.county;
+
+import com.taobao.cun.auge.flow.FlowContent;
+
+public interface TestAService {
+	FlowContent test1(String taskCode, Long id);
+	
+	FlowContent test2(String taskCode, Long id);
+}
