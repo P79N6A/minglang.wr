cbfa02c2295bf8f46860ef1a617ac3f7c857d36a
quanzhu.wangqz <quanzhu.wangqz@alibaba-inc.com>
2019-04-19 17:48:02 +0800

    服务站列表

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/store/bo/StoreReadBO.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/store/bo/StoreReadBO.java
@@ -1,11 +1,12 @@
 package com.taobao.cun.auge.store.bo;
 
-import java.util.List;
-
 import com.taobao.cun.auge.common.PageDto;
+import com.taobao.cun.auge.dal.domain.CuntaoStore;
 import com.taobao.cun.auge.store.dto.StoreDto;
 import com.taobao.cun.auge.store.dto.StoreQueryPageCondition;
 import com.taobao.cun.auge.store.dto.StoreStatus;
+
+import java.util.List;
 
 public interface StoreReadBO {
 	StoreDto getStoreDtoByStationId(Long stationId);
@@ -32,4 +33,18 @@ public interface StoreReadBO {
 	List<StoreDto> getStoreBySharedStoreIds(List<Long> sharedStoreIds);
 	
 	List<StoreDto> getStoreBySellerShareStoreIds(List<Long> sellerShareStoreId);
+
+	/**
+	 * 根据村点获得  CuntaoStore 数据
+	 * @param stationId
+	 * @return
+	 */
+	public CuntaoStore getCuntaoStoreByStationId(Long stationId);
+
+	/**
+	 * 根据storeId获得  CuntaoStore 数据
+	 * @param sharedStoreId
+	 * @return
+	 */
+	public CuntaoStore getCuntaoStoreBySharedStoreId(Long sharedStoreId);
 }
