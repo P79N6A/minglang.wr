73b4654c3623dbdbe46223960fd4137b9943ac93
haiou.chenho <haiou.chenho@alibaba-inc.com>
2019-07-03 10:32:15 +0800

    门店当面付

--- station-bundle-impl/src/main/java/com/taobao/cun/auge/alipay/service/impl/AlipayFaceToFacePaymentServiceImpl.java
+++ station-bundle-impl/src/main/java/com/taobao/cun/auge/alipay/service/impl/AlipayFaceToFacePaymentServiceImpl.java
@@ -195,15 +195,15 @@ public class AlipayFaceToFacePaymentServiceImpl implements AlipayFaceToFacePayme
         if(cuntaoQualification!=null){
             String certUrl=DOLWAN_QUAN_IMAGE_URI+cuntaoQualification.getQualiPic();
             AntMerchantExpandIndirectImageUploadRequest request = new AntMerchantExpandIndirectImageUploadRequest();
-            String pic=cuntaoQualification.getQualiPic();
-            if(StringUtil.isNotBlank(pic)){
-                if(pic.indexOf(".png")>0||pic.indexOf(".PNG")>0) {
-                    request.setImageType("png");
-                }
-                else if(pic.indexOf(".jpg")>0||pic.indexOf(".JPG")>0){
-                    request.setImageType("jpg");
-                }
-            }
+   //         String pic=cuntaoQualification.getQualiPic();
+//            if(StringUtil.isNotBlank(pic)){
+//                if(pic.indexOf(".png")>0||pic.indexOf(".PNG")>0) {
+//                    request.setImageType("png");
+//                }
+//                else if(pic.indexOf(".jpg")>0||pic.indexOf(".JPG")>0){
+//                    request.setImageType("jpg");
+//                }
+//            }
             saveToFile(certUrl,"/home/admin/"+cuntaoQualification.getQualiPic());
 
             FileItem ImageContent = new FileItem("/home/admin/"+cuntaoQualification.getQualiPic());
